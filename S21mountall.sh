36b36899e3218a562de46b5bcd388f68f3ccbcb8
Eddie Cai <eddie.cai.linux@gmail.com>
2019-04-09 14:22:17 +0800

    S21mountall.sh: sync to buildroot
    Change-Id: I4c3fd10cae047dd55fe02a553d85fa0192f44124
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- S21mountall.sh
+++ S21mountall.sh
@@ -5,6 +5,9 @@
 
 MISC_DEV=/dev/block/by-name/misc
 
+BUSYBOX_MOUNT_OPTS="loop (a|)sync (no|)atime (no|)diratime (no|)relatime (no|)dev (no|)exec (no|)suid (r|)shared (r|)slave (r|)private (un|)bindable (r|)bind move remount ro"
+NTFS_3G_MOUNT_OPTS="ro uid=[0-9]* gid=[0-9]* umask=[0-9]* fmask=[0-9]* dmask=[0-9]*"
+
 check_tool()
 {
 	TOOL=$1
@@ -14,6 +17,17 @@ check_tool()
 
 	[ -n "$CONFIG" ] && echo "You may need to enable $CONFIG"
 	return 1
+}
+
+remount_part()
+{
+	mountpoint -q $MOUNT_POINT || return
+
+	if touch $MOUNT_POINT &>/dev/null; then
+		[ "$1" = ro ] && mount -o remount,ro $MOUNT_POINT
+	else
+		[ "$1" = rw ] && mount -o remount,rw $MOUNT_POINT
+	fi
 }
 
 format_part()
@@ -42,36 +56,6 @@ format_part()
 			false
 			;;
 	esac
-}
-
-need_resize()
-{
-	case $FSGROUP in
-		ext2)
-			check_tool dumpe2fs BR2_PACKAGE_E2FSPROGS || return 1
-			LABEL=$(dumpe2fs -h $DEV | grep "name:")
-			;;
-		vfat)
-			check_tool fatlabel BR2_PACKAGE_DOSFSTOOLS_FATLABE || return 1
-			LABEL=$(fatlabel $DEV)
-			;;
-		ntfs)
-			check_tool ntfslabel BR2_PACKAGE_NTFS_3G_NTFSPROGS || return 1
-			LABEL=$(ntfslabel $DEV)
-			;;
-		*)
-			echo Unsupported file system $FSTYPE for $DEV
-			return 1
-			;;
-	esac
-
-	if [ $? -ne 0 ]; then
-		echo "Wrong fs type($FSTYPE) for $DEV"
-		return 1
-	fi
-
-	# Use volume label to mark resized
-	[ "$(echo $LABEL|xargs -n 1|tail -1)" != "$PART_NAME" ]
 }
 
 format_resize()
@@ -104,7 +88,23 @@ format_resize()
 	umount $TEMP
 }
 
-resize_fatresize()
+resize_ext2()
+{
+	check_tool resize2fs BR2_PACKAGE_E2FSPROGS_RESIZE2FS || return 1
+
+	# Force using online resize, see:
+	# https://bugs.launchpad.net/ubuntu/+source/e2fsprogs/+bug/1796788.
+	TEMP=$(mktemp -d)
+	$MOUNT $DEV $TEMP || return 1
+
+	resize2fs $DEV && tune2fs $DEV -L $PART_NAME
+	RET=$?
+
+	umount $TEMP
+	return $RET
+}
+
+resize_vfat()
 {
 	check_tool fatresize BR2_PACKAGE_FATRESIZE || return 1
 
@@ -120,53 +120,31 @@ resize_fatresize()
 		MAX_SIZE=$(($MAX_SIZE - 512 * 1024))
 
 		# Try to resize with fatresize, not always work
-		fatresize -s ${MAX_SIZE} $DEV && return
+		if fatresize -s ${MAX_SIZE} $DEV; then
+			fatlabel $DEV $PART_NAME
+			return 0
+		fi
 	done
-
 	return 1
-}
-
-resize_ext2()
-{
-	check_tool resize2fs BR2_PACKAGE_E2FSPROGS_RESIZE2FS || return
-
-	# Force using online resize, see:
-	# https://bugs.launchpad.net/ubuntu/+source/e2fsprogs/+bug/1796788.
-	TEMP=$(mktemp -d)
-	$MOUNT $DEV $TEMP || return
-	resize2fs $DEV && tune2fs $DEV -L $PART_NAME
-	umount $TEMP
-}
-
-resize_vfat()
-{
-	check_tool fatlabel BR2_PACKAGE_DOSFSTOOLS_FATLABE || return
-	resize_fatresize && fatlabel $DEV $PART_NAME
 }
 
 resize_ntfs()
 {
-	check_tool ntfsresize BR2_PACKAGE_NTFS_3G_NTFSPROGS || return
+	check_tool ntfsresize BR2_PACKAGE_NTFS_3G_NTFSPROGS || return 1
 	echo y | ntfsresize -f $DEV && ntfslabel $DEV $PART_NAME
 }
 
 resize_part()
 {
-	need_resize || return
+	# Already resized
+	# Use volume label to mark resized
+	[ "$FS_LABEL" = "$PART_NAME" ] && return
 
 	echo "Resizing $DEV($FSTYPE)"
 
-	case $FSGROUP in
-		ext2|vfat|ntfs)
-			eval resize_$FSGROUP
-			;;
-		*)
-			echo Unsupported file system $FSTYPE for $DEV
-			return
-			;;
-	esac
-
-	need_resize || return
+	# Resize needs read-write
+	remount_part rw
+	eval resize_$FSGROUP && return
 
 	# Fallback to format resize
 	[ ! "$IS_ROOTDEV" ] && format_resize
@@ -185,12 +163,13 @@ done_oem_command()
 
 handle_oem_command()
 {
-	[ "$IS_ROOTDEV" ] && return
 	[ "$OEM_CMD" ] || return
 
 	for cmd in $OEM_CMD; do
 		case $cmd in
 			cmd_wipe_$PART_NAME)
+				[ "$IS_ROOTDEV" ] && continue
+
 				echo "OEM: $cmd - Wiping $DEV"
 				format_part && done_oem_command $cmd
 				;;
@@ -198,10 +177,95 @@ handle_oem_command()
 	done
 }
 
+convert_mount_opts()
+{
+	for opt in $@; do
+		echo $OPTS|grep -woE $opt
+	done | tr "\n" ","
+}
+
+prepare_part()
+{
+	case $FSTYPE in
+		ext[234])
+			FSGROUP=ext2
+			FSCK_CONFIG=BR2_PACKAGE_E2FSPROGS_FSCK
+			;;
+		msdos|fat|vfat)
+			FSGROUP=vfat
+			FSCK_CONFIG=BR2_PACKAGE_DOSFSTOOLS_FSCK_FAT
+			;;
+		ntfs)
+			FSGROUP=ntfs
+			FSCK_CONFIG=BR2_PACKAGE_NTFS_3G_NTFSPROGS
+			;;
+		*)
+			echo "Unsupported file system $FSTYPE for $DEV"
+			return 1
+	esac
+
+	case $FSGROUP in
+		ext2)
+			MOUNT="busybox mount"
+			MOUNT_OPTS=$(convert_mount_opts "$BUSYBOX_MOUNT_OPTS")
+
+			check_tool dumpe2fs BR2_PACKAGE_E2FSPROGS || return 1
+			LABEL=$(dumpe2fs -h $DEV 2>/dev/null| grep "name:")
+			;;
+		vfat)
+			MOUNT="busybox mount"
+			MOUNT_OPTS=$(convert_mount_opts "$BUSYBOX_MOUNT_OPTS")
+
+			check_tool fatlabel BR2_PACKAGE_DOSFSTOOLS_FATLABE || return 1
+			LABEL=$(fatlabel $DEV)
+			;;
+		ntfs)
+			MOUNT=ntfs-3g
+			check_tool ntfs-3g BR2_PACKAGE_NTFS_3G || return 1
+			MOUNT_OPTS=$(convert_mount_opts "$NTFS_3G_MOUNT_OPTS")
+
+			check_tool ntfslabel BR2_PACKAGE_NTFS_3G_NTFSPROGS || return 1
+			LABEL=$(ntfslabel $DEV)
+			;;
+		*)
+			echo Unsupported file system $FSTYPE for $DEV
+			return 1
+			;;
+	esac
+
+	if [ $? -ne 0 ]; then
+		echo "Wrong fs type($FSTYPE) for $DEV"
+		return 1
+	fi
+
+	FS_LABEL="$(echo $LABEL|xargs -n 1|tail -1)"
+
+	MOUNT_OPTS=${MOUNT_OPTS:+" -o ${MOUNT_OPTS%,}"}
+
+	# Try to umount the mounted partitions
+	[ "$IS_ROOTDEV" ] || umount $MOUNT_POINT &>/dev/null
+	mountpoint -q $MOUNT_POINT || return 0
+
+	MOUNTED_RO_RW=$(touch $MOUNT_POINT &>/dev/null && echo rw || echo ro)
+}
+
+check_part()
+{
+	[ "$SKIP_FSCK" -o "$PASS" -eq 0 ] && return
+	echo "Checking $DEV($FSTYPE)"
+
+	check_tool fsck.$FSGROUP $FSCK_CONFIG || return
+
+	# Fsck rootfs needs read-only
+	[ "$IS_ROOTDEV" ] && remount_part ro
+
+	fsck.$FSGROUP -y $DEV
+}
+
 do_part()
 {
 	# Not enough args
-	[ $# -lt 3 ] && return
+	[ $# -lt 6 ] && return
 
 	# Ignore comments
 	echo $1 |grep -q "^#" && return
@@ -209,11 +273,17 @@ do_part()
 	DEV=$1
 	MOUNT_POINT=$2
 	FSTYPE=$3
+	OPTS=$4
+	PASS=$6 # Skip fsck when pass is 0
+
 	IS_ROOTDEV=$(echo $MOUNT_POINT | grep -w '/')
 
 	# Find real dev for root dev
 	if [ "$IS_ROOTDEV" ];then
 		DEV=$(mountpoint -n /|cut -d ' ' -f 1)
+
+		# Fallback to the by-name link
+		[ "$DEV" ] || DEV=/dev/block/by-name/rootfs
 	fi
 
 	DEV=$(realpath $DEV 2>/dev/null)
@@ -221,60 +291,46 @@ do_part()
 	# Unknown device
 	[ -b "$DEV" ] || return
 
+	echo "Handling $DEV $MOUNT_POINT $FSTYPE $OPTS $PASS"
+
 	SYS_PATH=/sys/class/block/${DEV##*/}
 	MAX_SIZE=$(( $(cat ${SYS_PATH}/size) * 512))
 	PART_NAME=$(grep PARTNAME ${SYS_PATH}/uevent | cut -d '=' -f 2)
 
-	echo "Handling $DEV $MOUNT_POINT $FSTYPE"
-
-	# Skip mounted partitions
-	if [ ! "$IS_ROOTDEV" ] && mountpoint -q $MOUNT_POINT; then
-		echo "Already mounted $DEV($MOUNT_POINT)"
-		return
-	fi
-
-	MOUNT="mount -t $FSTYPE"
-	case $FSTYPE in
-		ext[234])
-			FSGROUP=ext2
-			check_tool fsck.$FSGROUP BR2_PACKAGE_E2FSPROGS_FSCK || return
-			;;
-		msdos|fat|vfat)
-			FSGROUP=vfat
-			check_tool fsck.$FSGROUP BR2_PACKAGE_DOSFSTOOLS_FSCK_FAT || return
-			;;
-		ntfs)
-			FSGROUP=ntfs
-			MOUNT=ntfs-3g
-			check_tool fsck.$FSGROUP BR2_PACKAGE_NTFS_3G_NTFSPROGS || return
-			;;
-		*)
-			echo "Unsupported file system $FSTYPE for $DEV"
-			return
-	esac
-
 	# Handle OEM commands for current partition
 	handle_oem_command
 
+	# Check fs types and setup check/mount tools
+	prepare_part || return
+
+	# Resize partition if needed
 	resize_part
+
+	# Check and repair
+	check_part
+
+	# Restore ro/rw
+	remount_part $MOUNTED_RO_RW
 
 	# Done with rootdev
 	[ "$IS_ROOTDEV" ] && return
 
-	if [ ! "$SKIP_FSCK" ]; then
-		echo "Checking $DEV($FSTYPE)"
-		fsck.$FSGROUP -y $DEV
+	# Done with mounted partitions
+	if mountpoint -q $MOUNT_POINT; then
+		echo "Already mounted $DEV($MOUNT_POINT)"
+		return
 	fi
 
-	echo "Mounting $DEV($FSTYPE)"
-	$MOUNT $DEV $MOUNT_POINT && return
+	echo "Mounting $DEV($FSTYPE) on $MOUNT_POINT ${MOUNT_OPTS:+with$MOUNT_OPTS}"
+	$MOUNT $DEV $MOUNT_POINT $MOUNT_OPTS && return
 	[ "$AUTO_MKFS" ] || return
 
 	echo "Failed to mount $DEV, try to format it"
-	format_part && $MOUNT $DEV $MOUNT_POINT
+	format_part && \
+		$MOUNT $DEV $MOUNT_POINT $MOUNT_OPTS
 }
 
-prepare_mount()
+prepare_mountall()
 {
 	OEM_CMD=$(strings $MISC_DEV | grep "^cmd_" | xargs)
 	[ "$OEM_CMD" ] && echo "Note: Fount OEM commands - $OEM_CMD"
@@ -306,7 +362,9 @@ mountall()
 
 	echo "Will now mount all partitions in /etc/fstab"
 
-	prepare_mount
+	# Set environments for mountall
+	prepare_mountall
+
 	while read LINE;do
 		do_part $LINE
 	done < /etc/fstab
