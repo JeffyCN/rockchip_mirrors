2a313e9fc5b3562588e2e52be5aafd836e54ba17
Jeffy Chen <jeffy.chen@rock-chips.com>
2021-06-15 10:45:00 +0800

    rockchip: Drop out-dated adbd related packages
    Use android-tools instead.
    
    Change-Id: Ic37c1d1a81532eed1fdfdd3c1d55d8b5b430b024
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/rockchip/liblog/liblog.mk
+++
@@ -1,8 +0,0 @@
-# Rockchip's liblog porting from Android
-# Author : Cody Xie <cody.xie@rock-chips.com>
-
-LIBLOG_SITE = $(TOPDIR)/../external/liblog
-LIBLOG_SITE_METHOD = local
-LIBLOG_INSTALL_STAGING = YES
-
-$(eval $(cmake-package))
