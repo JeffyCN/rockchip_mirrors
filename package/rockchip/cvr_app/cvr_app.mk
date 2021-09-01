df86512ffcb48b3ce42f2e98cc8f34e17ac2959a
Zhichao Yu <zhichao.yu@rock-chips.com>
2021-09-01 15:58:09 +0800

    package: rockchip: cvr_app: add dependency
    Signed-off-by: Zhichao Yu <zhichao.yu@rock-chips.com>
    Change-Id: I2300a1e05e04107c7c3df57e79cda9379c4d7380

--- package/rockchip/cvr_app/cvr_app.mk
+++ package/rockchip/cvr_app/cvr_app.mk
@@ -1,6 +1,9 @@
 CVR_APP_SITE = $(TOPDIR)/../app/cvr_app
 CVR_APP_SITE_METHOD = local
 
+# add dependencies
+CVR_APP_DEPENDENCIES = linux-rga lvgl
+
 CVR_APP_INSTALL_STAGING = YES
 
 ifeq ($(BR2_PACKAGE_RK_OEM), y)
