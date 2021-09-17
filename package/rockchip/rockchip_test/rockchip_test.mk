6f007bf7ca20d3aec2326a9000a2a717a5d6d4f0
ZiHan Huang <zack.huang@rock-chips.com>
2021-09-17 15:40:49 +0800

    rockchip_test: add rk356x npu test
    Signed-off-by: ZiHan Huang <zack.huang@rock-chips.com>
    Change-Id: I1490618d581b7fd0a3f1a0af6779a49b2b8bbeb4

--- package/rockchip/rockchip_test/rockchip_test.mk
+++ package/rockchip/rockchip_test/rockchip_test.mk
@@ -1,7 +1,7 @@
 # add test tool for rockchip platform
 # Author : Hans Yang <yhx@rock-chips.com>
 
-ROCKCHIP_TEST_VERSION = 20201125
+ROCKCHIP_TEST_VERSION = 20210918
 ROCKCHIP_TEST_SITE_METHOD = local
 ROCKCHIP_TEST_SITE = $(TOPDIR)/package/rockchip/rockchip_test/src
 ROCKCHIP_TEST_LICENSE = Apache V2.0
@@ -11,6 +11,7 @@ ifeq ($(BR2_PACKAGE_RKNPU)$(BR2_PACKAGE_RKNN_DEMO),y)
 define ROCKCHIP_TEST_INSTALL_NPU_TARGET_CMDS
 	rm -rf ${TARGET_DIR}/rockchip_test/npu
 	cp -rf $(@D)/npu_${ARCH}  ${TARGET_DIR}/rockchip_test/npu
+	cp -rf $(@D)/npu2_aarch64  ${TARGET_DIR}/rockchip_test/npu2
 endef
 ROCKCHIP_TEST_POST_INSTALL_TARGET_HOOKS = ROCKCHIP_TEST_INSTALL_NPU_TARGET_CMDS
 endif
@@ -19,6 +20,7 @@ define ROCKCHIP_TEST_INSTALL_TARGET_CMDS
 	cp -rf  $(@D)/rockchip_test  ${TARGET_DIR}/
 	cp -rf $(@D)/rockchip_test_${ARCH}/* ${TARGET_DIR}/rockchip_test/ || true
 	$(INSTALL) -D -m 0755 $(@D)/rockchip_test/auto_reboot/S99_auto_reboot $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 0755 $(@D)/npu2_aarch64/rknn_mobilenet_demo $(TARGET_DIR)/rockchip_test/npu2/
 endef
 
 $(eval $(generic-package))
