30616d5cf0e268872235dd7017b15834c0315595
Zhichao Yu <zhichao.yu@rock-chips.com>
2021-10-30 15:44:17 +0800

    package: rockchip: cvr_app: fix dependency of rkfsmk
    Signed-off-by: Zhichao Yu <zhichao.yu@rock-chips.com>
    Change-Id: I9c42217e77ea07fb67a61596696721cc0cf0e2fe

--- package/rockchip/cvr_app/cvr_app.mk
+++ package/rockchip/cvr_app/cvr_app.mk
@@ -2,7 +2,7 @@ CVR_APP_SITE = $(TOPDIR)/../app/cvr_app
 CVR_APP_SITE_METHOD = local
 
 # add dependencies
-CVR_APP_DEPENDENCIES = camera_engine_rkaiq rkadk linux-rga lvgl
+CVR_APP_DEPENDENCIES = rkfsmk camera_engine_rkaiq rkadk linux-rga lvgl
 
 CVR_APP_INSTALL_STAGING = YES
 
