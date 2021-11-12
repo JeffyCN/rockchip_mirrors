JeffyCN/rockchip_mirrors

commit 8aa73ca79c076f41c4b36afccc54678f030ce7bb

package/python-pyparsing: bump to version 2.4.7
Sync with master:
  26bd4c9062 package/python-pyparsing: bump to version 2.4.7

Signed-off-by: Shunqian Zheng <zhengsq@rock-chips.com>
Change-Id: I2e6eac388bb4e6ebcfdae9458cad394ced2e9065

package/python-pyparsing/python-pyparsing.mk
@@ -4,12 +4,11 @@
 #
 ################################################################################
 
-PYTHON_PYPARSING_VERSION = 2.1.10
+PYTHON_PYPARSING_VERSION = 2.4.7
 PYTHON_PYPARSING_SOURCE = pyparsing-$(PYTHON_PYPARSING_VERSION).tar.gz
-PYTHON_PYPARSING_SITE = https://pypi.python.org/packages/38/bb/bf325351dd8ab6eb3c3b7c07c3978f38b2103e2ab48d59726916907cd6fb
+PYTHON_PYPARSING_SITE = https://files.pythonhosted.org/packages/c1/47/dfc9c342c9842bbe0036c7f763d2d6686bcf5eb1808ba3e170afdb282210
 PYTHON_PYPARSING_LICENSE = MIT
 PYTHON_PYPARSING_LICENSE_FILES = LICENSE
 PYTHON_PYPARSING_SETUP_TYPE = setuptools
 
-$(eval $(host-python-package))
 $(eval $(python-package))

package/python-pyparsing/Config.in
@@ -7,4 +7,4 @@ config BR2_PACKAGE_PYTHON_PYPARSING
 	  module provides a library of classes that client code uses
 	  to construct the grammar directly in Python code.
 
-	  http://pyparsing.wikispaces.com/
+	  https://github.com/pyparsing/pyparsing/

package/python-pyparsing/python-pyparsing.hash
@@ -1,3 +1,5 @@
-# md5 from https://pypi.python.org/pypi/pyparsing/json, sha256 locally computed
-md5 065908b92904e0d3634eb156f44cc80e pyparsing-2.1.10.tar.gz
-sha256 811c3e7b0031021137fc83e051795025fcb98674d07eb8fe922ba4de53d39188 pyparsing-2.1.10.tar.gz
+# md5, sha256 from https://pypi.org/pypi/pyparsing/json
+md5  f0953e47a0112f7a65aec2305ffdf7b4  pyparsing-2.4.7.tar.gz
+sha256  c203ec8783bf771a155b207279b9bccb8dea02d8f0c9e5f8ead507bc3246ecc1  pyparsing-2.4.7.tar.gz
+# Locally computed sha256 checksums
+sha256  10d5120a16805804ffda8b688c220bfb4e8f39741b57320604d455a309e01972  LICENSE

commit 4f56f117597d5ca77888c702c8e475723ac9cf04

packages: including host-python-pyparsing
Fixes: 8aa73ca79c ("package/python-pyparsing: bump to version 2.4.7")

More details at redmine #324623.

Signed-off-by: Shunqian Zheng <zhengsq@rock-chips.com>
Change-Id: I1ce07ff0143947357b7c9e179c5f2edeefea5150

package/python-pyparsing/python-pyparsing.mk
@@ -11,4 +11,5 @@ PYTHON_PYPARSING_LICENSE = MIT
 PYTHON_PYPARSING_LICENSE_FILES = LICENSE
 PYTHON_PYPARSING_SETUP_TYPE = setuptools
 
+$(eval $(host-python-package))
 $(eval $(python-package))