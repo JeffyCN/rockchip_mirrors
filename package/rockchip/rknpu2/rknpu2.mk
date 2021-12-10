19e3f61ea0863a87abc787cfb4006406770fe3ef
ZiHan Huang <zack.huang@rock-chips.com>
2021-12-10 14:37:17 +0800

    package/rknpu2: Add 3588 compilation options to rknpu2.mk
    3588NPU1.2.0 release, mk files need to add rK3588 compilation option
    
    Signed-off-by: ZiHan Huang <zack.huang@rock-chips.com>
    Change-Id: I9723f172f40ae8f61874eebb53db701455ece9dd

--- package/rockchip/rknpu2/rknpu2.mk
+++ package/rockchip/rknpu2/rknpu2.mk
@@ -3,7 +3,7 @@
 # rknpu2
 #
 ################################################################################
-RKNPU2_VERSION = 1.1.0
+RKNPU2_VERSION = 1.2.0
 RKNPU2_SITE_METHOD = local
 RKNPU2_SITE = $(TOPDIR)/../external/rknpu2
 RKNPU2_INSTALL_STAGING = YES
@@ -14,15 +14,23 @@ else
 NPU_PLATFORM_ARCH = aarch64
 endif
 
+ifeq ($(BR2_PACKAGE_RK3588), y)
+NPU_PLATFORM_INFO = RK3588
+endif
+
+ifeq ($(BR2_PACKAGE_RK356X),y)
+NPU_PLATFORM_INFO = RK356X
+endif
+
 define RKNPU2_INSTALL_STAGING_CMDS
     mkdir -p $(STAGING_DIR)/usr/include/rknn
-    $(INSTALL) -D -m 0644 $(@D)/Linux/librknn_api/include/rknn_api.h $(STAGING_DIR)/usr/include/rknn/rknn_api.h
+    $(INSTALL) -D -m 0644 $(@D)/runtime/$(NPU_PLATFORM_INFO)/Linux/librknn_api/include/rknn_api.h $(STAGING_DIR)/usr/include/rknn/rknn_api.h
 endef
 
 define RKNPU2_INSTALL_TARGET_CMDS
 
-    cp -r $(@D)/Linux/rknn_server/${NPU_PLATFORM_ARCH}/usr/bin/* $(STAGING_DIR)/usr/bin/
-    cp -r $(@D)/Linux/librknn_api/${NPU_PLATFORM_ARCH}/* $(STAGING_DIR)/usr/lib/
+    cp -r $(@D)/runtime/$(NPU_PLATFORM_INFO)/Linux/rknn_server/${NPU_PLATFORM_ARCH}/usr/bin/* $(STAGING_DIR)/usr/bin/
+    cp -r $(@D)/runtime/$(NPU_PLATFORM_INFO)/Linux/librknn_api/${NPU_PLATFORM_ARCH}/* $(STAGING_DIR)/usr/lib/
 endef
 
 $(eval $(generic-package))
