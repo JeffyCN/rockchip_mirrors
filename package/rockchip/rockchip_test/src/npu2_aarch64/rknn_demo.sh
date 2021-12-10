8436f850cae301cd9fbd003d22bfa29c3fa4bcf1
ZiHan Huang <zack.huang@rock-chips.com>
2021-12-10 15:09:53 +0800

    rockchip_test: add rk3588 rknpu2 64bit and 32 bit test.
    Signed-off-by: ZiHan Huang <zack.huang@rock-chips.com>
    Change-Id: I8169d925553590590f379f9a4790248380d12c3e

--- package/rockchip/rockchip_test/src/npu2_aarch64/rknn_demo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=/rockchip_test/npu2/lib
-
-/rockchip_test/npu2/rknn_mobilenet_demo /rockchip_test/npu2/model/mobilenet_v1.rknn /rockchip_test/npu2/model/dog_224x224.jpg;
