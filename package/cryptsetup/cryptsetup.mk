304db34341c7f0c28784c0c78c08a95582513971
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-01-14 10:07:20 +0800

    Bump util-linux/systemd/libglib2/fontconfig/eudev/cryptsetup
    There's a circular dependency in util-linux and related packages, which
    has been fixed in buildroot upstream.
    
    Bump related packages to buildroot upstream versions to fix that issue:
    27856dfd1a package/networkd-dispatcher: new package
    f8a5f4ecd8 package/kodi-pvr-mythtv: bump version to 19.0.6-Matrix
    95717ff91b package/kodi-pvr-argustv: bump version to 19.2.1-Matrix
    9197cfa5a5 package/kodi-inputstream-adaptive: bump version to 19.0.2-Matrix
    60e27aee9b package/freerdp: bump to version 2.5.0
    1b5e90c320 package/tinycbor: disable -Werror
    
    Change-Id: I9cbcb0f48ca83d03c829c2095d55252c04d4638b
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/cryptsetup/cryptsetup.mk
+++ package/cryptsetup/cryptsetup.mk
@@ -4,16 +4,21 @@
 #
 ################################################################################
 
-CRYPTSETUP_VERSION_MAJOR = 2.0
-CRYPTSETUP_VERSION = $(CRYPTSETUP_VERSION_MAJOR).0
+CRYPTSETUP_VERSION_MAJOR = 2.4
+CRYPTSETUP_VERSION = $(CRYPTSETUP_VERSION_MAJOR).2
 CRYPTSETUP_SOURCE = cryptsetup-$(CRYPTSETUP_VERSION).tar.xz
 CRYPTSETUP_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/cryptsetup/v$(CRYPTSETUP_VERSION_MAJOR)
-CRYPTSETUP_DEPENDENCIES = lvm2 popt util-linux host-pkgconf json-c \
+CRYPTSETUP_DEPENDENCIES = \
+	lvm2 popt host-pkgconf json-c libargon2 \
+	$(if $(BR2_PACKAGE_LIBICONV),libiconv) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_LIBS),util-linux-libs,util-linux) \
 	$(TARGET_NLS_DEPENDENCIES)
 CRYPTSETUP_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (library)
 CRYPTSETUP_LICENSE_FILES = COPYING COPYING.LGPL
+CRYPTSETUP_CPE_ID_VENDOR = cryptsetup_project
 CRYPTSETUP_INSTALL_STAGING = YES
 CRYPTSETUP_CONF_ENV += LDFLAGS="$(TARGET_LDFLAGS) $(TARGET_NLS_LIBS)"
+CRYPTSETUP_CONF_OPTS += --enable-blkid --enable-libargon2
 
 # cryptsetup uses libgcrypt by default, but can be configured to use OpenSSL
 # or kernel crypto modules instead
@@ -28,6 +33,19 @@ else
 CRYPTSETUP_CONF_OPTS += --with-crypto_backend=kernel
 endif
 
+ifeq ($(BR2_PACKAGE_LIBSSH),y)
+CRYPTSETUP_DEPENDENCIES += \
+	$(if $(BR2_PACKAGE_ARGP_STANDALONE),argp-standalone) \
+	libssh
+CRYPTSETUP_CONF_OPTS += --enable-ssh-token
+else
+CRYPTSETUP_CONF_OPTS += --disable-ssh-token
+endif
+
+ifeq ($(BR2_PACKAGE_SYSTEMD),y)
+CRYPTSETUP_CONF_OPTS += --with-tmpfilesdir=/usr/lib/tmpfiles.d
+endif
+
 HOST_CRYPTSETUP_DEPENDENCIES = \
 	host-pkgconf \
 	host-lvm2 \
@@ -36,7 +54,11 @@ HOST_CRYPTSETUP_DEPENDENCIES = \
 	host-json-c \
 	host-openssl
 
-HOST_CRYPTSETUP_CONF_OPTS = --with-crypto-backend=openssl
+HOST_CRYPTSETUP_CONF_OPTS = --with-crypto_backend=openssl \
+	--disable-kernel_crypto \
+	--disable-ssh-token \
+	--enable-blkid \
+	--with-tmpfilesdir=no
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
