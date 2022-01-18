7f3ff8e6f4aa8a56bdd05f47a4f9365c9588139b
ZiHan Huang <zack.huang@rock-chips.com>
2022-01-18 15:24:06 +0800

    rockchip_test: fix model path
    Signed-off-by: ZiHan Huang <zack.huang@rock-chips.com>
    Change-Id: I8938ad4be0ae375ec2107f778822431bfdf69178

--- package/rockchip/rockchip_test/src/RK356X/npu2_arm/rknn_demo.sh
+++ package/rockchip/rockchip_test/src/RK356X/npu2_arm/rknn_demo.sh
@@ -2,4 +2,4 @@
 
 export LD_LIBRARY_PATH=/rockchip_test/npu2/lib
 
-/rockchip_test/npu2/rknn_mobilenet_demo /rockchip_test/npu2/model/mobilenet_v1.rknn /rockchip_test/npu2/model/dog_224x224.jpg;
+/rockchip_test/npu2/rknn_mobilenet_demo /rockchip_test/npu2/model/RK356X/mobilenet_v1.rknn /rockchip_test/npu2/model/dog_224x224.jpg;
--- package/rockchip/rockchip_test/src/RK3588/npu2_aarch64/npu_stress_test.sh
+++ package/rockchip/rockchip_test/src/RK3588/npu2_aarch64/npu_stress_test.sh
@@ -4,5 +4,5 @@ export LD_LIBRARY_PATH=/rockchip_test/npu2/lib
 
 while true
 do   
-/rockchip_test/npu2/rknn_mobilenet_demo /rockchip_test/npu2/model/mobilenet_v1.rknn /rockchip_test/npu2/model/dog_224x224.jpg;
+/rockchip_test/npu2/rknn_mobilenet_demo /rockchip_test/npu2/model/RK3588/mobilenet_v1.rknn /rockchip_test/npu2/model/dog_224x224.jpg;
 done
