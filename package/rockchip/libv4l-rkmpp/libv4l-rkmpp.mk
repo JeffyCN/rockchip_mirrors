053a33e6e12c8f68bdf14be7fae2906ace68d878
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-01-28 10:34:08 +0800

    packages: Add libv4l-rkmpp
    Change-Id: Ibead81093c9e61b5c6c6c8a2134c30446f34dc34
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

---
+++ package/rockchip/libv4l-rkmpp/libv4l-rkmpp.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# libv4l-rkmpp
+#
+################################################################################
+
+LIBV4L_RKMPP_SITE = git://github.com/JeffyCN/libv4l-rkmpp.git
+LIBV4L_RKMPP_VERSION = 8562eb7cddeebc4527ffe42139a89736f1c8c08a
+LIBV4L_RKMPP_SITE_METHOD = git
+LIBV4L_RKMPP_AUTORECONF = YES
+
+LIBV4L_RKMPP_LICENSE = LGPL-2.1
+LIBV4L_RKMPP_LICENSE_FILES = COPYING
+
+LIBV4L_RKMPP_DEPENDENCIES = libv4l rockchip-mpp
+
+$(eval $(autotools-package))
