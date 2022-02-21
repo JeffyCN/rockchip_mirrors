7fd00fd9af90316c370dd21f4124e383e102a1cd
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-02-21 16:34:35 +0800

    xcursor-themes & xcursor-transparent-theme: Drop dependencies of x11r7 and xcursor
    They are just themes.
    
    Change-Id: I5aa1763c069f49541b05eb12a35dfcff975577c8
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/xcursor-transparent-theme/xcursor-transparent-theme.mk
+++ package/xcursor-transparent-theme/xcursor-transparent-theme.mk
@@ -6,12 +6,13 @@
 
 XCURSOR_TRANSPARENT_THEME_VERSION = 0.1.1
 XCURSOR_TRANSPARENT_THEME_SITE = http://downloads.yoctoproject.org/releases/matchbox/utils
-XCURSOR_TRANSPARENT_THEME_DEPENDENCIES = xlib_libXcursor host-xapp_xcursorgen
 XCURSOR_TRANSPARENT_THEME_LICENSE = GPL-2.0
 XCURSOR_TRANSPARENT_THEME_LICENSE_FILES = COPYING
 
+XCURSOR_TRANSPARENT_THEME_DEPENDENCIES = host-xapp_xcursorgen
+
 define XCURSOR_TRANSPARENT_THEME_ICONS_DEFAULT_CONFIG_INSTALL
-	$(INSTALL) -m 0755 -D package/x11r7/xcursor-transparent-theme/index.theme \
+	$(INSTALL) -m 0755 -D $(XCURSOR_TRANSPARENT_THEME_PKGDIR)/index.theme \
 		$(TARGET_DIR)/usr/share/icons/default/index.theme
 endef
 
