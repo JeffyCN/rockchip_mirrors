c6954d10bd5032693320a6d80a9c7fb41e031b16
Caesar Wang <wxt@rock-chips.com>
2022-03-03 10:43:43 +0800

    package/rockchip: update rockchip-test
    Updated on 2022/03/03
    
    move rockchiip-test/src to external/rockchip-test
    
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Change-Id: Ib94ccc314b209f42cb69a48cac81593b387a0b86

--- package/rockchip/rockchip-test/rockchip-test.mk
+++ package/rockchip/rockchip-test/rockchip-test.mk
@@ -1,14 +1,15 @@
 # add test tool for rockchip platform
 
-ROCKCHIP_TEST_VERSION = 20220218
+ROCKCHIP_TEST_SITE = $(TOPDIR)/../external/rockchip-test
+ROCKCHIP_TEST_VERSION = master
 ROCKCHIP_TEST_SITE_METHOD = local
-ROCKCHIP_TEST_SITE = $(TOPDIR)/package/rockchip/rockchip-test/src
 ROCKCHIP_TEST_LICENSE = ROCKCHIP
 ROCKCHIP_TEST_LICENSE_FILES = LICENSE
 
 define ROCKCHIP_TEST_INSTALL_TARGET_CMDS
-	cp -rf  $(@D)/rockchip-test  ${TARGET_DIR}/
-	$(INSTALL) -D -m 0755 $(@D)/rockchip-test/auto_reboot/S99-auto-reboot $(TARGET_DIR)/etc/init.d/
+	mkdir -p ${TARGET_DIR}/rockchip-test
+	cp -rf  $(@D)/*  ${TARGET_DIR}/rockchip-test/
+	$(INSTALL) -D -m 0755 $(@D)/auto_reboot/S99-auto-reboot $(TARGET_DIR)/etc/init.d/
 endef
 
 $(eval $(generic-package))
--- package/rockchip/rockchip-test/src/rockchip-test/video/test_gst_video.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gst-launch-1.0 filesrc location=/oem/SampleVideo_1280x720_5mb.mp4 ! qtdemux ! h264parse ! mppvideodec ! waylandsink
-#gst-launch-1.0 playbin uri=file:///oem/SampleVideo_1280x720_5mb.mp4 audio-sink=fakesink
