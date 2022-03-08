9de0784665758304c18219735cad9f5a098915ae
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-03-08 16:18:01 +0800

    gstreamer1: Bump to buildroot upstream's 1.20.0
    Bump to:
    1cd8faa8d3 package/go: security bump to 1.17.8
    
    Change-Id: Ie10293b01d9611cd4920f7d69a1e586e0004c4f7
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/gstreamer1/gst1-libav/gst1-libav.mk
+++ package/gstreamer1/gst1-libav/gst1-libav.mk
@@ -8,4 +8,6 @@ ifeq ($(BR2_PACKAGE_GSTREAMER1_14),y)
 include $(pkgdir)/1_14.inc
 else ifeq ($(BR2_PACKAGE_GSTREAMER1_18),y)
 include $(pkgdir)/1_18.inc
+else ifeq ($(BR2_PACKAGE_GSTREAMER1_20),y)
+include $(pkgdir)/1_20.inc
 endif
--- package/gstreamer1/gst1-plugins-base/gst1-plugins-base.mk
+++ package/gstreamer1/gst1-plugins-base/gst1-plugins-base.mk
@@ -8,9 +8,10 @@ ifeq ($(BR2_PACKAGE_LINUX_RGA),y)
 GST1_PLUGINS_BASE_DEPENDENCIES += linux-rga
 endif
 
-
 ifeq ($(BR2_PACKAGE_GSTREAMER1_14),y)
 include $(pkgdir)/1_14.inc
 else ifeq ($(BR2_PACKAGE_GSTREAMER1_18),y)
 include $(pkgdir)/1_18.inc
+else ifeq ($(BR2_PACKAGE_GSTREAMER1_20),y)
+include $(pkgdir)/1_20.inc
 endif
