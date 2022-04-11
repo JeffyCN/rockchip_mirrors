7a53d13939be3e6d777aa9d4cff98eb4425c0ce8
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-04-11 18:35:09 +0800

    qlauncher: Update build rules
    1/ Switch to qmake-package
    2/ Add S50launcher
    
    Change-Id: I93dfb629340e71b95e18318e90504b1849d75fa8
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/rockchip/qlauncher/qlauncher.mk
+++ package/rockchip/qlauncher/qlauncher.mk
@@ -8,20 +8,17 @@ QLAUNCHER_VERSION = 1.0
 QLAUNCHER_SITE = $(TOPDIR)/../app/QLauncher
 QLAUNCHER_SITE_METHOD = local
 
-QLAUNCHER_LICENSE = Apache V2.0
-QLAUNCHER_LICENSE_FILES = NOTICE
+QLAUNCHER_LICENSE = ROCKCHIP
+QLAUNCHER_LICENSE_FILES = LICENSE
 
-define QLAUNCHER_CONFIGURE_CMDS
-	cd $(@D); $(TARGET_MAKE_ENV) $(HOST_DIR)/bin/qmake
-endef
-
-define QLAUNCHER_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
-endef
-
+# TODO: Add install rules in .pro
 define QLAUNCHER_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/QLauncher	$(TARGET_DIR)/usr/bin/QLauncher
-	$(INSTALL) -D -m 0755 $(@D)/S50launcher	$(TARGET_DIR)/etc/init.d/
+        $(INSTALL) -D -m 0755 $(@D)/QLauncher $(TARGET_DIR)/usr/bin/QLauncher
 endef
 
-$(eval $(generic-package))
+define QLAUNCHER_INSTALL_INIT_SYSV
+$(INSTALL) -D -m 755 $(QLAUNCHER_PKGDIR)/S50launcher \
+	$(TARGET_DIR)/etc/init.d/S50launcher
+endef
+
+$(eval $(qmake-package))
