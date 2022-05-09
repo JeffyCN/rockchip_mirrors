36812beb4a171389c5221cdf229e4f9681d8a00b
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-05-09 11:36:38 +0800

    qt5: Support eglfs again
    Drop weston.config in defconfig to switch to eglfs.
    
    Change-Id: Ia594bcd9aa6e719ae6e3a655e72b9503b4be01da
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/qt5/qt5base/qt5base.mk
+++ package/qt5/qt5base/qt5base.mk
@@ -215,6 +215,15 @@ QT5BASE_CONFIGURE_OPTS += $(if $(QT5BASE_DEFAULT_QPA),-qpa $(QT5BASE_DEFAULT_QPA
 ifeq ($(BR2_PACKAGE_QT5BASE_EGLFS),y)
 QT5BASE_CONFIGURE_OPTS += -eglfs
 QT5BASE_DEPENDENCIES   += libegl
+
+# Avoid conflict with Rockchip BSP kernel's logo.
+define QT5BASE_INSTALL_TARGET_ENV
+	echo "export QT_QPA_EGLFS_ALWAYS_SET_MODE=1" > $(@D)/qt_eglfs.sh
+	$(INSTALL) -D -m 0644 $(@D)/qt_eglfs.sh \
+		$(TARGET_DIR)/etc/profile.d/qt_eglfs.sh
+endef
+QT5BASE_POST_INSTALL_TARGET_HOOKS += QT5BASE_INSTALL_TARGET_ENV
+
 else
 QT5BASE_CONFIGURE_OPTS += -no-eglfs
 endif
