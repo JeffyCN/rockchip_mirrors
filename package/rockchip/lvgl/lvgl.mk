d925643fb4790450f804c3503f89821884cfc6b4
Jinkun Hong <jinkun.hong@rock-chips.com>
2022-06-06 15:09:30 +0800

    package: lvgl: init v8.2.0
    Change-Id: I77c60b4233601970e3cfba0c6426d5c28b27873c
    Signed-off-by: Jinkun Hong <jinkun.hong@rock-chips.com>

--- package/rockchip/lvgl/lvgl.mk
+++ package/rockchip/lvgl/lvgl.mk
@@ -1,6 +1,12 @@
-LVGL_SITE = $(TOPDIR)/../external/lvgl
-LVGL_SITE_METHOD = local
+################################################################################
+#
+# lvgl
+#
+################################################################################
 
+LVGL_VERSION = 8.2.0
+LVGL_SOURCE = v$(LVGL_VERSION).tar.gz
+LVGL_SITE = https://github.com/lvgl/lvgl/archive/refs/tags
 LVGL_INSTALL_STAGING = YES
 
 ifeq ($(BR2_PACKAGE_RK_OEM), y)
