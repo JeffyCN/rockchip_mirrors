4c52aca54c2a619bed91df48df7e5de0d45e5d9e
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-06-27 09:46:58 +0800

    stressapptest: Fix download error
    Change-Id: If1bf0a3e404b7f4f44f2e59c1b229dadb225fb3e
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/stressapptest/stressapptest.mk
+++ package/stressapptest/stressapptest.mk
@@ -1,11 +1,14 @@
 ################################################################################
 #
-# stress app test
+# stressapptest
 #
 ################################################################################
+
 STRESSAPPTEST_SITE = https://github.com/stressapptest/stressapptest.git
-STRESS_LICENSE = Apache-2.0
-STRESS_LICENSE_FILES = COPYING
-STRESSAPPTEST_VERSION = master
+STRESSAPPTEST_SITE_METHOD = git
+STRESSAPPTEST_VERSION = 6714c57d0d67f5a2a7a9987791af6729289bf64e
+
+STRESSAPPTEST_LICENSE = Apache-2.0
+STRESSAPPTEST_LICENSE_FILES = NOTICE
 
 $(eval $(autotools-package))
