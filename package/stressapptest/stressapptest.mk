f27163b16d67ba85d30278cf0ed05242dc4c8a27
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-06-27 09:47:47 +0800

    stressapptest: Sync with 2021 SDK
    Change-Id: I3346112d7b467afad36eceb73a75392e5197b8a3
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/stressapptest/stressapptest.mk
+++ package/stressapptest/stressapptest.mk
@@ -1,11 +1,14 @@
 ################################################################################
 #
-# stress app test
+# stressapptest
 #
 ################################################################################
-STRESSAPPTEST_SITE = https://github.com/stressapptest/stressapptest
-STRESSAPPTEST_VERSION = master
+
+STRESSAPPTEST_SITE = https://github.com/stressapptest/stressapptest.git
 STRESSAPPTEST_SITE_METHOD = git
-STRESSAPPTEST_GIT_SUBMODULES = YES
+STRESSAPPTEST_VERSION = 6714c57d0d67f5a2a7a9987791af6729289bf64e
+
+STRESSAPPTEST_LICENSE = Apache-2.0
+STRESSAPPTEST_LICENSE_FILES = NOTICE
 
 $(eval $(autotools-package))
