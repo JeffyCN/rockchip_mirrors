b601846a36891f0ebd6245b62d2c405254c3bb8e
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-07-29 16:33:11 +0800

    libv4l-rkmpp: Bump to 1.4.0
    Change-Id: Ib27b4e919fcf460d3d8b103b551488c3b24407d8
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/rockchip/libv4l-rkmpp/libv4l-rkmpp.mk
+++ package/rockchip/libv4l-rkmpp/libv4l-rkmpp.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBV4L_RKMPP_SITE = https://github.com/JeffyCN/libv4l-rkmpp.git
-LIBV4L_RKMPP_VERSION = 8562eb7cddeebc4527ffe42139a89736f1c8c08a
+LIBV4L_RKMPP_VERSION = 0971b134c5e5606366c3912e37684ded8116a8b2
 LIBV4L_RKMPP_SITE_METHOD = git
 LIBV4L_RKMPP_AUTORECONF = YES
 
