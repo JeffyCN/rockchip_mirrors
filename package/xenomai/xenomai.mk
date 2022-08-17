b3ac8a1063c89671d1802cfc110050abdd6f7fa5
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-08-17 19:31:21 +0800

    xenomai: Support 3.2/3.1/3.0
    We need 3.2/3.1 for aarch64.
    
    Change-Id: I39b0ed9ea44c535460233b7ce86ebe0d103b3526
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/xenomai/xenomai.mk
+++ package/xenomai/xenomai.mk
@@ -9,20 +9,19 @@ ifeq ($(BR2_PACKAGE_XENOMAI_CUSTOM_TARBALL),y)
 XENOMAI_TARBALL = $(call qstrip,$(BR2_PACKAGE_XENOMAI_CUSTOM_TARBALL_URL))
 XENOMAI_SOURCE = $(notdir $(XENOMAI_TARBALL))
 XENOMAI_SITE = $(patsubst %/,%,$(dir $(XENOMAI_TARBALL)))
+# Exclude from the hash check
+BR_NO_CHECK_HASH_FOR += $(XENOMAI_SOURCE)
 else ifeq ($(BR2_PACKAGE_XENOMAI_CUSTOM_GIT),y)
 XENOMAI_SITE = $(call qstrip,$(BR2_PACKAGE_XENOMAI_REPOSITORY))
 XENOMAI_SITE_METHOD = git
+# Exclude from the hash check
+BR_NO_CHECK_HASH_FOR += $(XENOMAI_SOURCE)
 else
 XENOMAI_SOURCE = xenomai-$(XENOMAI_VERSION).tar.bz2
-XENOMAI_SITE = http://xenomai.org/downloads/xenomai/stable
+XENOMAI_SITE = https://source.denx.de/Xenomai/xenomai/-/archive/$(XENOMAI_VERSION)
 endif
 # We're patching configure.ac
 XENOMAI_AUTORECONF = YES
-
-# Exclude all from the hash check, but the latest version.
-ifeq ($(BR2_PACKAGE_XENOMAI)$(BR2_PACKAGE_XENOMAI_LATEST_VERSION),y)
-BR_NO_CHECK_HASH_FOR += $(XENOMAI_SOURCE)
-endif
 
 XENOMAI_LICENSE = GPL-2.0+ with exception (headers), LGPL-2.1+ (libraries), GPL-2.0+ (kernel), GFDL-1.2+ (docs), GPL-2.0 (ipipe patch, can driver)
 # GFDL is not included but refers to gnu.org
