efc81be26b106d462d4a2c8e13cc5ce7acbdd6d8
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-08-24 14:04:05 +0800

    Fix a few extract error
    The newest version needs to use ${PKG}_DL_DIR.
    
    Change-Id: I92f02651e98250a2102dc18fe120416bd3dc6b63
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/cracklib/cracklib.mk
+++ package/cracklib/cracklib.mk
@@ -4,12 +4,14 @@
 #
 ################################################################################
 
-CRACKLIB_VERSION = 2.9.6
-CRACKLIB_SITE = https://github.com/cracklib/cracklib/releases/download/cracklib-$(CRACKLIB_VERSION)
+CRACKLIB_VERSION = 2.9.7
+CRACKLIB_SITE = https://github.com/cracklib/cracklib/releases/download/v$(CRACKLIB_VERSION)
 CRACKLIB_LICENSE = LGPL-2.1
 CRACKLIB_LICENSE_FILES = COPYING.LIB
+CRACKLIB_CPE_ID_VENDOR = cracklib_project
 CRACKLIB_INSTALL_STAGING = YES
-CRACKLIB_DEPENDENCIES = host-cracklib
+CRACKLIB_DEPENDENCIES = host-cracklib $(TARGET_NLS_DEPENDENCIES)
+CRACKLIB_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
 
 ifeq ($(BR2_PACKAGE_ZLIB),y)
 CRACKLIB_CONF_OPTS += --with-zlib
@@ -18,9 +20,12 @@ else
 CRACKLIB_CONF_OPTS += --without-zlib
 endif
 
-ifeq ($(BR2_PACKAGE_PYTHON),y)
+ifeq ($(BR2_PACKAGE_PYTHON3),y)
 CRACKLIB_CONF_OPTS += --with-python
-CRACKLIB_DEPENDENCIES += python
+CRACKLIB_CONF_ENV += \
+	ac_cv_path_PYTHON=$(HOST_DIR)/bin/python3 \
+	am_cv_python_version=$(PYTHON3_VERSION_MAJOR)
+CRACKLIB_DEPENDENCIES += python3
 else
 CRACKLIB_CONF_OPTS += --without-python
 endif
@@ -29,7 +34,7 @@ HOST_CRACKLIB_CONF_OPTS += --without-python --without-zlib
 
 ifeq ($(BR2_PACKAGE_CRACKLIB_FULL_DICT),y)
 CRACKLIB_EXTRA_DOWNLOADS = cracklib-words-$(CRACKLIB_VERSION).gz
-CRACKLIB_DICT_SOURCE = $(DL_DIR)/cracklib-words-$(CRACKLIB_VERSION).gz
+CRACKLIB_DICT_SOURCE = $(CRACKLIB_DL_DIR)/cracklib-words-$(CRACKLIB_VERSION).gz
 else
 CRACKLIB_DICT_SOURCE = $(@D)/dicts/cracklib-small
 endif
