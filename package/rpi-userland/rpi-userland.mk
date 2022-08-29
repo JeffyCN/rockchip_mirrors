d8d6ff2feaed0fdcf520b3daea1818848142c0f8
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-08-29 15:35:38 +0800

    {intel,ti,rpi,sunxi}-*: Bump to buildroot upstream
    Bump to:
    dbfc9f9f67 package/xenomai: fix indentation
    
    Change-Id: I1158c623ed460f8a5fbeb77f5cef16c8b9c203e5
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,29 +4,17 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = a7976021a89451de0d008aa48f16c4e88872899b
+RPI_USERLAND_VERSION = 14b90ff9d9f031391a299e6e006965d02bfd1bb1
 RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3-Clause
 RPI_USERLAND_LICENSE_FILES = LICENCE
 RPI_USERLAND_INSTALL_STAGING = YES
-RPI_USERLAND_CONF_OPTS = -DVMCS_INSTALL_PREFIX=/usr \
-	-DCMAKE_C_FLAGS="$(TARGET_CFLAGS) \
-		-DVCFILED_LOCKFILE=\\\"/var/run/vcfiled.pid\\\""
+RPI_USERLAND_CONF_OPTS = -DVMCS_INSTALL_PREFIX=/usr
 
 RPI_USERLAND_PROVIDES = libegl libgles libopenmax libopenvg
 
-ifeq ($(BR2_PACKAGE_RPI_USERLAND_START_VCFILED),y)
-define RPI_USERLAND_INSTALL_INIT_SYSV
-	$(INSTALL) -m 0755 -D package/rpi-userland/S94vcfiled \
-		$(TARGET_DIR)/etc/init.d/S94vcfiled
-endef
-define RPI_USERLAND_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 644 package/rpi-userland/vcfiled.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/vcfiled.service
-	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
-	ln -sf ../../../../usr/lib/systemd/system/vcfiled.service \
-		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/vcfiled.service
-endef
+ifeq ($(BR2_PACKAGE_LIBEXECINFO),y)
+RPI_USERLAND_DEPENDENCIES += libexecinfo
 endif
 
 ifeq ($(BR2_PACKAGE_RPI_USERLAND_HELLO),y)
@@ -54,9 +42,6 @@ RPI_USERLAND_CONF_OPTS += -DALL_APPS=OFF
 endif # BR2_PACKAGE_RPI_USERLAND_HELLO
 
 define RPI_USERLAND_POST_TARGET_CLEANUP
-	rm -f $(TARGET_DIR)/etc/init.d/vcfiled
-	rm -f $(TARGET_DIR)/usr/share/install/vcfiled
-	rmdir --ignore-fail-on-non-empty $(TARGET_DIR)/usr/share/install
 	rm -Rf $(TARGET_DIR)/usr/src
 endef
 RPI_USERLAND_POST_INSTALL_TARGET_HOOKS += RPI_USERLAND_POST_TARGET_CLEANUP
