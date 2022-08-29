bdd357a0c994d36633a06a96a0b8b471a36652ee
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-08-29 15:35:38 +0800

    kodi*: Bump to buildroot upstream
    Bump to:
    dbfc9f9f67 package/xenomai: fix indentation
    
    Change-Id: Id5af35ec29fccf59491535f580ceb8e81fd8a595
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/kodi-pvr-iptvsimple/kodi-pvr-iptvsimple.mk
+++ package/kodi-pvr-iptvsimple/kodi-pvr-iptvsimple.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-# This cset is on the branch 'Krypton'
-# When Kodi is updated, then this should be updated to the corresponding branch
-KODI_PVR_IPTVSIMPLE_VERSION = 2.4.14-Krypton
+KODI_PVR_IPTVSIMPLE_VERSION = 19.2.1-Matrix
 KODI_PVR_IPTVSIMPLE_SITE = $(call github,kodi-pvr,pvr.iptvsimple,$(KODI_PVR_IPTVSIMPLE_VERSION))
 KODI_PVR_IPTVSIMPLE_LICENSE = GPL-2.0+
-KODI_PVR_IPTVSIMPLE_LICENSE_FILES = src/client.h
-KODI_PVR_IPTVSIMPLE_DEPENDENCIES = kodi-platform
+KODI_PVR_IPTVSIMPLE_LICENSE_FILES = LICENSE.md
+KODI_PVR_IPTVSIMPLE_DEPENDENCIES = kodi pugixml xz zlib
 
 $(eval $(cmake-package))
--- package/kodi-pvr-njoy/kodi-pvr-njoy.mk
+++ package/kodi-pvr-njoy/kodi-pvr-njoy.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-# This cset is on the branch 'Krypton'
-# When Kodi is updated, then this should be updated to the corresponding branch
-KODI_PVR_NJOY_VERSION = 2.4.3-Krypton
+KODI_PVR_NJOY_VERSION = 19.0.1-Matrix
 KODI_PVR_NJOY_SITE = $(call github,kodi-pvr,pvr.njoy,$(KODI_PVR_NJOY_VERSION))
 KODI_PVR_NJOY_LICENSE = GPL-2.0+
-KODI_PVR_NJOY_LICENSE_FILES = src/client.h
-KODI_PVR_NJOY_DEPENDENCIES = kodi-platform
+KODI_PVR_NJOY_LICENSE_FILES = LICENSE.md
+KODI_PVR_NJOY_DEPENDENCIES = kodi tinyxml
 
 $(eval $(cmake-package))
