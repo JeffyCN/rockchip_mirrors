293dac722db720c88594b80ada7b85acf706d42d
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-01 10:07:46 +0800

    v*: Bump to buildroot upstream
    Bump to:
    dbfc9f9f67 package/xenomai: fix indentation
    
    With custom modifies:
    v4l2grab
    valgrind
    vkmark
    vulkan-headers
    vulkan-loader
    vulkan-tools
    
    Change-Id: I4376858d315927eae4bdaa54a01c0f8b81b0bbab
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/v4l2loopback/v4l2loopback.mk
+++ package/v4l2loopback/v4l2loopback.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-V4L2LOOPBACK_VERSION = v0.10.0
-V4L2LOOPBACK_SITE = $(call github,umlaeute,v4l2loopback,$(V4L2LOOPBACK_VERSION))
+V4L2LOOPBACK_VERSION = 0.12.5
+V4L2LOOPBACK_SITE = $(call github,umlaeute,v4l2loopback,v$(V4L2LOOPBACK_VERSION))
 V4L2LOOPBACK_LICENSE = GPL-2.0+
 V4L2LOOPBACK_LICENSE_FILES = COPYING
 
@@ -14,6 +14,16 @@ define V4L2LOOPBACK_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/utils/v4l2loopback-ctl $(TARGET_DIR)/usr/bin/v4l2loopback-ctl
 endef
 endif
+
+# CONFIG_MEDIA_SUPPORT depends on CONFIG_HAS_IOMEM, which is only
+# available when CONFIG_PCI=y on S390. CONFIG_VIDEO_DEV needs
+# CONFIG_I2C since Linux 5.18.
+define V4L2LOOPBACK_LINUX_CONFIG_FIXUPS
+	$(if $(BR2_s390x),$(call KCONFIG_ENABLE_OPT,CONFIG_PCI))
+	$(call KCONFIG_ENABLE_OPT,CONFIG_MEDIA_SUPPORT)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_I2C)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_VIDEO_DEV)
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
