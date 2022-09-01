4141785fbf695b322fe16c5ca9925b25cefd3d8d
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-01 10:07:46 +0800

    t*: Bump to buildroot upstream
    Bump to:
    dbfc9f9f67 package/xenomai: fix indentation
    
    With custom modifies:
    triggerhappy
    
    Change-Id: Idb3e593f09a7121dbb65e5c5d00cee4df3304b23
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/tinyxml2/tinyxml2.mk
+++ package/tinyxml2/tinyxml2.mk
@@ -4,22 +4,15 @@
 #
 ################################################################################
 
-TINYXML2_VERSION = 6.2.0
+TINYXML2_VERSION = 9.0.0
 TINYXML2_SITE = $(call github,leethomason,tinyxml2,$(TINYXML2_VERSION))
 TINYXML2_LICENSE = Zlib
-TINYXML2_LICENSE_FILES = readme.md
+TINYXML2_LICENSE_FILES = LICENSE.txt
 TINYXML2_INSTALL_STAGING = YES
+TINYXML2_CPE_ID_VENDOR = tinyxml2_project
 
 ifeq ($(BR2_STATIC_LIBS),y)
 TINYXML2_CONF_OPTS += -DBUILD_STATIC_LIBS=ON
 endif
-
-define TINYXML2_FIX_CONFIG_CMAKE_FILE
-	$(SED) 's%set(_IMPORT_PREFIX \"\/usr\")%set(_IMPORT_PREFIX \"$(STAGING_DIR)\/usr\")%' \
-		$(STAGING_DIR)/usr/lib/cmake/tinyxml2/tinyxml2Targets.cmake
-	$(SED) 's%INTERFACE_INCLUDE_DIRECTORIES \"\/usr\/include\"%INTERFACE_INCLUDE_DIRECTORIES \"$(STAGING_DIR)\/usr\/include\"%' \
-		$(STAGING_DIR)/usr/lib/cmake/tinyxml2/tinyxml2Targets.cmake
-endef
-TINYXML2_POST_INSTALL_STAGING_HOOKS += TINYXML2_FIX_CONFIG_CMAKE_FILE
 
 $(eval $(cmake-package))
