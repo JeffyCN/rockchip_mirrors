72f81d958ce6e510f0fcfe549c29a9bbf62b96fc
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-01 10:07:46 +0800

    c*: Bump to buildroot upstream
    Bump to:
    dbfc9f9f67 package/xenomai: fix indentation
    
    With custom modifies:
    cog
    chromium-wayland
    
    Change-Id: I8a06f7782587f377f3f67e6bbf9a750cbf1f20a9
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/clamav/clamav.mk
+++ package/clamav/clamav.mk
@@ -4,15 +4,20 @@
 #
 ################################################################################
 
-CLAMAV_VERSION = 0.99.3
+CLAMAV_VERSION = 0.103.7
 CLAMAV_SITE = https://www.clamav.net/downloads/production
 CLAMAV_LICENSE = GPL-2.0
 CLAMAV_LICENSE_FILES = COPYING COPYING.bzip2 COPYING.file COPYING.getopt \
 	COPYING.LGPL COPYING.llvm COPYING.lzma COPYING.pcre COPYING.regex \
 	COPYING.unrar COPYING.zlib
+CLAMAV_CPE_ID_VENDOR = clamav
+CLAMAV_SELINUX_MODULES = clamav
+# affects only Cisco devices
+CLAMAV_IGNORE_CVES += CVE-2016-1405
 CLAMAV_DEPENDENCIES = \
 	host-pkgconf \
-	libtool \
+	libcurl \
+	libmspack \
 	openssl \
 	zlib \
 	$(TARGET_NLS_DEPENDENCIES)
@@ -20,19 +25,29 @@ CLAMAV_DEPENDENCIES = \
 # mmap cannot be detected when cross-compiling, needed for mempool support
 CLAMAV_CONF_ENV = \
 	ac_cv_c_mmap_private=yes \
-	have_cv_ipv6=yes
+	have_cv_ipv6=yes \
+	OBJC=$(TARGET_CC)
 
-# UCLIBC_HAS_FTS is disabled, therefore disable fanotify (missing fts.h)
+ifeq ($(BR2_TOOLCHAIN_HAS_LIBATOMIC),y)
+CLAMAV_LIBS += -latomic
+endif
+
+ifeq ($(BR2_TOOLCHAIN_USES_GLIBC),)
+CLAMAV_DEPENDENCIES += musl-fts
+CLAMAV_LIBS += -lfts
+endif
+
+CLAMAV_CONF_ENV += LIBS="$(CLAMAV_LIBS)"
+
 CLAMAV_CONF_OPTS = \
 	--with-dbdir=/var/lib/clamav \
-	--with-ltdl-include=$(STAGING_DIR)/usr/include \
-	--with-ltdl-lib=$(STAGING_DIR)/usr/lib \
+	--with-libcurl=$(STAGING_DIR)/usr \
 	--with-openssl=$(STAGING_DIR)/usr \
+	--with-system-libmspack=$(STAGING_DIR)/usr \
 	--with-zlib=$(STAGING_DIR)/usr \
 	--disable-zlib-vcheck \
 	--disable-rpath \
 	--disable-clamav \
-	--disable-fanotify \
 	--disable-milter \
 	--disable-llvm \
 	--disable-clamdtop \
@@ -56,17 +71,11 @@ CLAMAV_CONF_OPTS += --without-libjson
 endif
 
 ifeq ($(BR2_PACKAGE_LIBXML2),y)
+CLAMAV_CONF_ENV += ac_cv_path_xmlconfig=$(STAGING_DIR)/usr/bin/xml2-config
 CLAMAV_CONF_OPTS += --with-xml=$(STAGING_DIR)/usr
 CLAMAV_DEPENDENCIES += libxml2
 else
 CLAMAV_CONF_OPTS += --disable-xml
-endif
-
-ifeq ($(BR2_PACKAGE_LIBCURL),y)
-CLAMAV_CONF_OPTS += --with-libcurl=$(STAGING_DIR)/usr
-CLAMAV_DEPENDENCIES += libcurl
-else
-CLAMAV_CONF_OPTS += --without-libcurl
 endif
 
 ifeq ($(BR2_PACKAGE_LIBICONV),y)
@@ -76,11 +85,21 @@ else
 CLAMAV_CONF_OPTS += --without-iconv
 endif
 
-ifeq ($(BR2_PACKAGE_PCRE),y)
+ifeq ($(BR2_PACKAGE_PCRE2),y)
+CLAMAV_CONF_OPTS += --with-pcre=$(STAGING_DIR)/usr
+CLAMAV_DEPENDENCIES += pcre2
+else ifeq ($(BR2_PACKAGE_PCRE),y)
 CLAMAV_CONF_OPTS += --with-pcre=$(STAGING_DIR)/usr
 CLAMAV_DEPENDENCIES += pcre
 else
 CLAMAV_CONF_OPTS += --without-pcre
+endif
+
+ifeq ($(BR2_INIT_SYSTEMD),y)
+CLAMAV_CONF_OPTS += --with-systemdsystemunitdir=/usr/lib/systemd/system
+CLAMAV_DEPENDENCIES += systemd
+else
+CLAMAV_CONF_OPTS += --with-systemdsystemunitdir=no
 endif
 
 $(eval $(autotools-package))
