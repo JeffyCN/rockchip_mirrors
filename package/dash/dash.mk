429dc8c428f3504245285f1cad8dceb620c1b8ff
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-01 10:07:46 +0800

    d*: Bump to buildroot upstream
    Bump to:
    dbfc9f9f67 package/xenomai: fix indentation
    
    With custom modifies:
    dbus
    dbus-cpp
    
    Change-Id: I040073e79f9a0562c9a925140907afb6385b7290
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/dash/dash.mk
+++ package/dash/dash.mk
@@ -4,14 +4,18 @@
 #
 ################################################################################
 
-DASH_VERSION = 0.5.9.1
+DASH_VERSION = 0.5.11.5
 DASH_SITE = http://gondor.apana.org.au/~herbert/dash/files
 DASH_LICENSE = BSD-3-Clause, GPL-2.0+ (mksignames.c)
 DASH_LICENSE_FILES = COPYING
 
+# dash does not build in parallel
+DASH_MAKE = $(MAKE1)
+
 ifeq ($(BR2_PACKAGE_LIBEDIT),y)
-DASH_DEPENDENCIES += libedit
+DASH_DEPENDENCIES += libedit host-pkgconf
 DASH_CONF_OPTS += --with-libedit
+DASH_CONF_ENV += LIBS=`pkg-config --libs libedit`
 
 # Enable line editing, Emacs style
 define DASH_INSTALL_PROFILE
@@ -24,7 +28,7 @@ DASH_CONF_OPTS += --without-libedit
 endif
 
 define DASH_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/src/dash $(TARGET_DIR)/bin/dash
+	$(INSTALL) -m 0755 -D $(@D)/src/dash $(TARGET_DIR)/bin/dash
 endef
 
 # Add /bin/dash to /etc/shells otherwise some login tools like dropbear
--- package/ding-libs/ding-libs.mk
+++ package/ding-libs/ding-libs.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-DING_LIBS_VERSION = 0_4_0
-DING_LIBS_SOURCE = ding_libs-$(DING_LIBS_VERSION).tar.xz
-DING_LIBS_SITE = https://git.fedorahosted.org/cgit/ding-libs.git/snapshot
+DING_LIBS_VERSION = 0.6.2
+DING_LIBS_SITE = \
+	https://github.com/SSSD/ding-libs/releases/download/$(DING_LIBS_VERSION)
 DING_LIBS_DEPENDENCIES = host-pkgconf \
 	$(TARGET_NLS_DEPENDENCIES) \
 	$(if $(BR2_PACKAGE_LIBICONV),libiconv)
