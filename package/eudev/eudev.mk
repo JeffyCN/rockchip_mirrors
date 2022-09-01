79578643b002c6d6d450aa7dcf6e0fdad451a1d7
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-01 10:07:46 +0800

    eudev: Bump to buildroot upstream
    Bump to:
    dbfc9f9f67 package/xenomai: fix indentation
    
    Change-Id: Icfeb26b1f8a53297c2461d1d867cee1b078b79ad
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/eudev/eudev.mk
+++ package/eudev/eudev.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-EUDEV_VERSION = 3.2.7
-EUDEV_SITE = http://dev.gentoo.org/~blueness/eudev
+EUDEV_VERSION = 3.2.11
+EUDEV_SITE = https://github.com/eudev-project/eudev/releases/download/v$(EUDEV_VERSION)
 EUDEV_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (libraries)
 EUDEV_LICENSE_FILES = COPYING
 EUDEV_INSTALL_STAGING = YES
@@ -49,9 +49,37 @@ define EUDEV_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 package/eudev/S10udev $(TARGET_DIR)/etc/init.d/S10udev
 endef
 
-# Required by default rules for input devices
-define EUDEV_USERS
-	- - input -1 * - - - Input device group
+# Avoid installing S10udev with openrc, as the service is started by a unit
+# from the udev-gentoo-scripts package.
+define EUDEV_INSTALL_INIT_OPENRC
+	@:
 endef
 
+HOST_EUDEV_DEPENDENCIES = host-gperf host-pkgconf
+
+HOST_EUDEV_CONF_OPTS = \
+	--prefix=/usr \
+	--sbindir=/sbin \
+	--libexecdir=/lib \
+	--with-rootlibdir=/lib \
+	--sysconfdir=/etc \
+	--disable-blkid \
+	--disable-introspection \
+	--disable-kmod \
+	--disable-manpages \
+	--disable-rule-generator \
+	--disable-selinux \
+	--enable-hwdb
+
+define HOST_EUDEV_INSTALL_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/src/udev/udevadm \
+		$(HOST_DIR)/bin/udevadm
+endef
+
+define HOST_EUDEV_BUILD_HWDB
+	$(HOST_DIR)/bin/udevadm hwdb --update --root $(TARGET_DIR)
+endef
+HOST_EUDEV_TARGET_FINALIZE_HOOKS += HOST_EUDEV_BUILD_HWDB
+
 $(eval $(autotools-package))
+$(eval $(host-autotools-package))
