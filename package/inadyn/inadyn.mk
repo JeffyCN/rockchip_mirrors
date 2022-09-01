afc6be53e54e48e1d214bb73b06e218f6bcb780f
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-01 10:07:46 +0800

    i*: Bump to buildroot upstream
    Bump to:
    dbfc9f9f67 package/xenomai: fix indentation
    
    With custom modifies:
    ifuse
    iniparser
    intel-wds
    irqbalance
    
    Change-Id: I185f36fe973e685fb2e199cc47dfdb45582da58e
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/inadyn/inadyn.mk
+++ package/inadyn/inadyn.mk
@@ -4,12 +4,11 @@
 #
 ################################################################################
 
-INADYN_VERSION = 2.2.1
+INADYN_VERSION = 2.9.1
 INADYN_SITE = https://github.com/troglobit/inadyn/releases/download/v$(INADYN_VERSION)
-INADYN_SOURCE = inadyn-$(INADYN_VERSION).tar.xz
 INADYN_LICENSE = GPL-2.0+
 INADYN_LICENSE_FILES = COPYING
-INADYN_DEPENDENCIES = host-pkgconf libconfuse libite
+INADYN_DEPENDENCIES = host-pkgconf libconfuse
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 INADYN_CONF_OPTS += --enable-openssl
@@ -32,11 +31,8 @@ define INADYN_INSTALL_INIT_SYSV
 endef
 
 define INADYN_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 644 package/inadyn/inadyn.service \
+	$(INSTALL) -D -m 644 $(@D)/inadyn.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/inadyn.service
-	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
-	ln -sf ../../../../usr/lib/systemd/system/inadyn.service \
-		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/inadyn.service
 endef
 
 $(eval $(autotools-package))
--- package/iperf/iperf.mk
+++ package/iperf/iperf.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-IPERF_VERSION = 2.0.10
+IPERF_VERSION = 2.1.6
 IPERF_SITE = http://downloads.sourceforge.net/project/iperf2
 IPERF_LICENSE = MIT-like
 IPERF_LICENSE_FILES = COPYING
-# patching configure.ac
-IPERF_AUTORECONF = YES
+IPERF_CPE_ID_VENDOR = iperf2_project
+IPERF_CPE_ID_PRODUCT = iperf2
 
 IPERF_CONF_OPTS = \
 	--disable-web100
