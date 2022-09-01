fd0dc915f8428d25e25ad725db9e692a583c6349
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-01 10:07:46 +0800

    jhead: Bump to buildroot upstream
    Bump to:
    dbfc9f9f67 package/xenomai: fix indentation
    
    With custom modifies:
    Support libjhead.
    
    Change-Id: If349ac1a00130e2bf7eced6b3e3ca8d88eb2e2a9
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/jhead/jhead.mk
+++ package/jhead/jhead.mk
@@ -4,30 +4,11 @@
 #
 ################################################################################
 
-JHEAD_VERSION = 3.00
-JHEAD_SITE = http://www.sentex.net/~mwandel/jhead
+JHEAD_VERSION = 3.06.0.1
+JHEAD_SITE = $(call github,Matthias-Wandel,jhead,$(JHEAD_VERSION))
 JHEAD_LICENSE = Public Domain
 JHEAD_LICENSE_FILES = readme.txt
+JHEAD_CPE_ID_VENDOR = jhead_project
+JHEAD_INSTALL_STAGING = YES
 
-ifeq ($(BR2_PACKAGE_LIBJHEAD),y)
-JHEAD_MAKE_ENV += CFLAGS+="-fPIC" LIBJHEAD=y
-
-define GEN_LIBJHEAD_C
-	cp -a $(@D)/jhead.c $(@D)/libjhead.c
-	sed -i '/^int main/,$$d' $(@D)/libjhead.c
-endef
-JHEAD_PRE_PATCH_HOOKS = GEN_LIBJHEAD_C
-endif
-
-define JHEAD_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) $(JHEAD_MAKE_ENV) -C $(@D)
-endef
-
-define JHEAD_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(@D)/jhead $(TARGET_DIR)/usr/bin/jhead
-	$(INSTALL) -m 0755 -D $(@D)/libjhead.so $(TARGET_DIR)/usr/lib/
-	$(INSTALL) -m 0755 -D $(@D)/libjhead.so $(STAGING_DIR)/usr/lib/
-	$(INSTALL) -m 0755 -D $(@D)/jhead.h $(STAGING_DIR)/usr/include/
-endef
-
-$(eval $(generic-package))
+$(eval $(meson-package))
