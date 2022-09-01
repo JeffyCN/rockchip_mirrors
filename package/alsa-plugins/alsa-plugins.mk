e6af959816a8bf172ba4f0ab0bf2749bf3edfcf3
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-01 10:07:46 +0800

    alsa*: Bump to buildroot upstream
    Bump to:
    dbfc9f9f67 package/xenomai: fix indentation
    
    With custom modifies:
    1/ alsa-lib: Local patches.
    2/ alsa-plugins: Support speex and pulseaudio.
    
    Change-Id: If6a1e659480b4d7aa58e3fcb54e5114ae78e8aa6
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/alsa-plugins/alsa-plugins.mk
+++ package/alsa-plugins/alsa-plugins.mk
@@ -1,22 +1,27 @@
-#T###############################################################################
+################################################################################
 #
-# alsa-utils
+# alsa-plugins
 #
 ################################################################################
 
-ALSA_PLUGINS_VERSION = 1.1.5
+ALSA_PLUGINS_VERSION = 1.2.6
 ALSA_PLUGINS_SOURCE = alsa-plugins-$(ALSA_PLUGINS_VERSION).tar.bz2
-ALSA_PLUGINS_SITE = ftp://ftp.alsa-project.org/pub/plugins
-ALSA_PLUGINS_LICENSE = GPLv2
+ALSA_PLUGINS_SITE = https://www.alsa-project.org/files/pub/plugins
+ALSA_PLUGINS_LICENSE = LGPL-2.1+
 ALSA_PLUGINS_LICENSE_FILES = COPYING
-ALSA_PLUGINS_INSTALL_STAGING = YES
 ALSA_PLUGINS_DEPENDENCIES = host-pkgconf alsa-lib
+
+ALSA_PLUGINS_CONF_OPTS = \
+	--disable-jack \
+	--disable-usbstream \
+	--disable-maemo-plugin \
+	--disable-maemo-resource-manager
 
 ifeq ($(BR2_PACKAGE_FFMPEG),y)
 ALSA_PLUGINS_DEPENDENCIES += ffmpeg
-ALSA_PLUGINS_CONF_OPTS += --enable-avcodec
+ALSA_PLUGINS_CONF_OPTS += --enable-libav
 else
-ALSA_PLUGINS_CONF_OPTS += --disable-avcodec
+ALSA_PLUGINS_CONF_OPTS += --disable-libav
 endif
 
 ifeq ($(BR2_PACKAGE_PULSEAUDIO),y)
@@ -24,8 +29,8 @@ ALSA_PLUGINS_DEPENDENCIES += pulseaudio
 ALSA_PLUGINS_CONF_OPTS += --enable-pulseaudio
 
 define ALSA_PLUGINS_DEFAULT_PULSEAUDIO
-	cd $(TARGET_DIR)/usr/share/alsa/alsa.conf.d && \
-		mv 99-pulseaudio-default.conf.example 99-pulseaudio-default.conf
+	cd $(TARGET_DIR) && \
+		cp etc/alsa/conf.d/99-pulseaudio-default.conf.example usr/share/alsa/alsa.conf.d/99-pulseaudio-default.conf
 endef
 ALSA_PLUGINS_POST_INSTALL_TARGET_HOOKS += ALSA_PLUGINS_DEFAULT_PULSEAUDIO
 
@@ -33,18 +38,20 @@ else
 ALSA_PLUGINS_CONF_OPTS += --disable-pulseaudio
 endif
 
-ifeq ($(BR2_PACKAGE_LIBSAMPLERATE),y)
-ALSA_PLUGINS_DEPENDENCIES += libsamplerate
-ALSA_PLUGINS_CONF_OPTS += --enable-samplerate
-else
-ALSA_PLUGINS_CONF_OPTS += --disable-samplerate
-endif
-
 ifeq ($(BR2_PACKAGE_SPEEX),y)
 ALSA_PLUGINS_DEPENDENCIES += speex
 ALSA_PLUGINS_CONF_OPTS += --with-speex=lib
 else
 ALSA_PLUGINS_CONF_OPTS += --with-speex=builtin
+endif
+
+ifeq ($(BR2_PACKAGE_LIBSAMPLERATE),y)
+ALSA_PLUGINS_CONF_OPTS += --enable-samplerate
+ALSA_PLUGINS_DEPENDENCIES += libsamplerate
+ALSA_PLUGINS_LICENSE += , GPL-2.0+ (samplerate plugin)
+ALSA_PLUGINS_LICENSE_FILES += COPYING.GPL
+else
+ALSA_PLUGINS_CONF_OPTS += --disable-samplerate
 endif
 
 $(eval $(autotools-package))
