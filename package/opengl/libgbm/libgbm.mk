5f15643b7fc046c65fabd6954acf9533f5f5a01c
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-01 10:07:46 +0800

    opengl*: Bump to buildroot upstream
    Bump to:
    dbfc9f9f67 package/xenomai: fix indentation
    
    Change-Id: I84f2259541d594c43db41b46690c3eb289c5ed7f
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/opengl/libgbm/libgbm.mk
+++ package/opengl/libgbm/libgbm.mk
@@ -4,4 +4,6 @@
 #
 ################################################################################
 
+# This package requires to install a gbm.pc which needs
+# to be provided by GBM providers.
 $(eval $(virtual-package))
