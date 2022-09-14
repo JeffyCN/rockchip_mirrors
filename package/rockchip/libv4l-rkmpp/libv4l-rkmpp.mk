da369425c1ec9e5aff9a078dcf61046d05a13d1f
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-14 10:40:10 +0800

    libv4l-rkmpp: Bump to 1.5.0
    Change-Id: Ib672127d3ff3210c8c4b2c7c6495c69d0ee8b467
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/rockchip/libv4l-rkmpp/libv4l-rkmpp.mk
+++ package/rockchip/libv4l-rkmpp/libv4l-rkmpp.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBV4L_RKMPP_SITE = https://github.com/JeffyCN/libv4l-rkmpp.git
-LIBV4L_RKMPP_VERSION = 0971b134c5e5606366c3912e37684ded8116a8b2
+LIBV4L_RKMPP_VERSION = 97b75e7b6d0328bf936c9c17c5d2d56ef2f25174
 LIBV4L_RKMPP_SITE_METHOD = git
 LIBV4L_RKMPP_AUTORECONF = YES
 
@@ -14,4 +14,11 @@ LIBV4L_RKMPP_LICENSE_FILES = COPYING
 
 LIBV4L_RKMPP_DEPENDENCIES = libv4l rockchip-mpp
 
-$(eval $(autotools-package))
+ifeq ($(BR2_PREFER_ROCKCHIP_RGA),y)
+LIBV4L_RKMPP_DEPENDENCIES += rockchip-rga
+LIBV4L_RKMPP_CONF_OPTS += -Drga=enabled
+else
+LIBV4L_RKMPP_CONF_OPTS += -Drga=disabled
+endif
+
+$(eval $(meson-package))
