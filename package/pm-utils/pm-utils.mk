ae4afc1138c83e127ce5bcf1acf8b4eb7b0210ea
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-20 12:08:29 +0800

    pm-utils: Support running pm-utils hooks for system-sleep
    Log saved in /var/log/pm-suspend.log.
    
    Change-Id: Ic878d1c869c8038b89a43fe584f41c93ec3d20de
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/pm-utils/pm-utils.mk
+++ package/pm-utils/pm-utils.mk
@@ -10,4 +10,10 @@ PM_UTILS_SOURCE = pm-utils-$(PM_UTILS_VERSION).tar.gz
 PM_UTILS_LICENSE = GPL-2.0+
 PM_UTILS_LICENSE_FILES = COPYING
 
+# Run pm-utils hooks for system-sleep
+define PM_UTILS_INSTALL_INIT_SYSTEMD
+        $(INSTALL) -m 0755 -D -t $(TARGET_DIR)/lib/systemd/system-sleep/ \
+		$(PM_UTILS_PKGDIR)/00pm-utils
+endef
+
 $(eval $(autotools-package))
