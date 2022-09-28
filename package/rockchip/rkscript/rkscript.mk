7fb212366f977f7eb09e738449cbd7b9f8329108
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-28 17:13:50 +0800

    rkscript: Refactor configs
    No functional changes.
    
    Change-Id: I5bcb8d030aa4d2a8761c441659a46babe611699b
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/rockchip/rkscript/rkscript.mk
+++ package/rockchip/rkscript/rkscript.mk
@@ -58,49 +58,66 @@ endef
 RKSCRIPT_POST_INSTALL_TARGET_HOOKS += RKSCRIPT_INSTALL_TARGET_UDEV_RULES
 endif
 
-ifneq ($(BR2_PACKAGE_RK356X)$(BR2_PACKAGE_RV1126_RV1109)$(BR2_PACKAGE_RK3308),)
+ifeq ($(BR2_PACKAGE_RKSCRIPT_IODOMAIN),y)
 define RKSCRIPT_INSTALL_INIT_SYSV_IODOMAIN
 	$(INSTALL) -m 0755 -D $(@D)/list-iodomain.sh $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 -D $(@D)/S98iodomain.sh $(TARGET_DIR)/etc/init.d/
 endef
 endif
 
-# The recovery will handle storages itself
-ifneq ($(BR2_PACKAGE_RECOVERY)|$(BR2_PACKAGE_RECOVERY_USE_UPDATEENGINE),y|)
+ifeq ($(BR2_PACKAGE_RKSCRIPT_MOUNTALL),y)
 define RKSCRIPT_INSTALL_INIT_SYSV_MOUNTALL
 	$(INSTALL) -m 0755 -D $(@D)/S21mountall.sh $(TARGET_DIR)/etc/init.d/
 endef
 endif
 
 ifeq ($(BR2_PACKAGE_RKSCRIPT_BOOTANIM),y)
+define RKSCRIPT_INSTALL_BOOTANIM
+	$(INSTALL) -m 0755 -D $(@D)/bootanim $(TARGET_DIR)/usr/bin/
+
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/etc/bootanim.d/
+	$(INSTALL) -m 0755 -D $(RKSCRIPT_PKGDIR)/gst-bootanim.sh \
+		$(TARGET_DIR)/etc/bootanim.d/
+endef
+
 define RKSCRIPT_INSTALL_INIT_SYSV_BOOTANIM
 	$(INSTALL) -m 0755 -D $(@D)/S31bootanim.sh $(TARGET_DIR)/etc/init.d/
 endef
 endif
 
-define RKSCRIPT_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -d $(TARGET_DIR)/etc/init.d/
+ifeq ($(BR2_PACKAGE_RKSCRIPT_USBDEVICE),y)
+define RKSCRIPT_INSTALL_USBDEVICE
 	$(INSTALL) -m 0755 -D $(@D)/usbdevice $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 -D $(@D)/S50usbdevice $(TARGET_DIR)/etc/init.d/
 
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/etc/init.d/
 	echo $(RKSCRIPT_USB_CONFIG) | xargs -n 1 > \
 		$(TARGET_DIR)/etc/init.d/.usb_config
+endef
 
-	$(INSTALL) -m 0755 -d $(TARGET_DIR)/etc/bootanim.d/
-	$(INSTALL) -m 0755 -D $(@D)/bootanim $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 -D $(RKSCRIPT_PKGDIR)/gst-bootanim.sh \
-		$(TARGET_DIR)/etc/bootanim.d/
+define RKSCRIPT_INSTALL_INIT_SYSV_USBDEVICE
+	$(INSTALL) -m 0755 -D $(@D)/S50usbdevice $(TARGET_DIR)/etc/init.d/
+endef
+
+define RKSCRIPT_INSTALL_INIT_SYSTEMD_USBDEVICE
+	$(INSTALL) -D -m 644 $(@D)/usbdevice.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/
+endef
+endif
+
+define RKSCRIPT_INSTALL_TARGET_CMDS
+	$(RKSCRIPT_INSTALL_BOOTANIM)
+	$(RKSCRIPT_INSTALL_USBDEVICE)
 endef
 
 define RKSCRIPT_INSTALL_INIT_SYSV
 	$(RKSCRIPT_INSTALL_INIT_SYSV_MOUNTALL)
 	$(RKSCRIPT_INSTALL_INIT_SYSV_IODOMAIN)
 	$(RKSCRIPT_INSTALL_INIT_SYSV_BOOTANIM)
+	$(RKSCRIPT_INSTALL_INIT_SYSV_USBDEVICE)
 endef
 
 define RKSCRIPT_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 644 $(@D)/usbdevice.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/
+	$(RKSCRIPT_INSTALL_INIT_SYSTEMD_USBDEVICE)
 endef
 
 $(eval $(generic-package))
