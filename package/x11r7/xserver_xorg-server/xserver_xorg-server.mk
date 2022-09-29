3b1726bbcae95a28995542cd96ccb069c2284d69
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-29 18:43:35 +0800

    xserver-xorg-server: Bump to 21.1.4_2022_09_29
    Support bootanim and freezing display.
    
    Change-Id: Ie59b8f36ddb5b2cd529185baba28031ac7c47814
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/x11r7/xserver_xorg-server/xserver_xorg-server.mk
+++ package/x11r7/xserver_xorg-server/xserver_xorg-server.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XSERVER_XORG_SERVER_VERSION = 21.1.4_2022_09_01
+XSERVER_XORG_SERVER_VERSION = 21.1.4_2022_09_29
 XSERVER_XORG_SERVER_SITE = $(call github,JeffyCN,xorg-xserver,$(XSERVER_XORG_SERVER_VERSION))
 XSERVER_XORG_SERVER_LICENSE = MIT
 XSERVER_XORG_SERVER_LICENSE_FILES = COPYING
