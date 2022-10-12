6e5356f7480dca279cf9de356d45aa725b4120cd
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-10-12 19:08:13 +0800

    rkscript: Use environment to config usbdevice
    Major changes:
    1/ Use /etc/profile.d/usbdevice.sh to config usbdevice.
    2/ Support more USB functions.
    
    Change-Id: Icc1332c4c5fd840d95cc277c228a14f674feb0b6
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

---
+++ package/rockchip/rkscript/usbdevice.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# The env variables below can be overridden
+
+export USB_FUNCS=
+export UMS_FILE=/userdata/ums_shared.img
+export UMS_SIZE=256M
+export UMS_FSTYPE=vfat
+export UMS_MOUNT=0
+export UMS_MOUNTPOINT=/mnt/ums
+export UMS_RO=0
