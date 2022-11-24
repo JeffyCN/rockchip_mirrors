76337dc844d94ceed4d0de55ee15bc19bb9fce10
Zhichao Yu <zhichao.yu@rock-chips.com>
2022-11-24 11:10:52 +0800

    package: ipcweb-backend: remove rk_oem build support
    The rk_oem has been deprecated, so let's remove it.
    
    Signed-off-by: Zhichao Yu <zhichao.yu@rock-chips.com>
    Change-Id: I8c1314761d1ceb0ea3b8c1196944032442232cf0

--- package/rockchip/ipcweb-backend/ipcweb-backend.mk
+++ package/rockchip/ipcweb-backend/ipcweb-backend.mk
@@ -27,25 +27,12 @@ IPCWEB_BACKEND_DEPENDENCIES += librkdb libgdbus libIPCProtocol
 IPCWEB_BACKEND_WWW_DIR = $(@D)/www
 endif
 
-ifeq ($(BR2_PACKAGE_RK_OEM), y)
-IPCWEB_BACKEND_INSTALL_TARGET_OPTS = DESTDIR=$(BR2_PACKAGE_RK_OEM_INSTALL_TARGET_DIR) install/fast
-IPCWEB_BACKEND_DEPENDENCIES += rk_oem
-IPCWEB_BACKEND_CONF_OPTS += -DIPCWEBBACKEND_INSTALL_ON_OEM_PARTITION=ON
-IPCWEB_BACKEND_TARGET_INSTALL_DIR = $(BR2_PACKAGE_RK_OEM_INSTALL_TARGET_DIR)
-
-define IPCWEB_BACKEND_INSTALL_TARGET_CMDS
-	rm -rf $(IPCWEB_BACKEND_TARGET_INSTALL_DIR)/www
-	cp -rfp $(IPCWEB_BACKEND_WWW_DIR) $(IPCWEB_BACKEND_TARGET_INSTALL_DIR)/www
-	mkdir -p $(IPCWEB_BACKEND_TARGET_INSTALL_DIR)/www/cgi-bin/
-	cp -rfp $(@D)/src/entry.cgi $(IPCWEB_BACKEND_TARGET_INSTALL_DIR)/www/cgi-bin/
-endef
-else
 define IPCWEB_BACKEND_INSTALL_TARGET_CMDS
 	rm -rf $(TARGET_DIR)/usr/www
+	mkdir -p $(TARGET_DIR)/usr/www
 	cp -rfp $(IPCWEB_BACKEND_WWW_DIR) $(TARGET_DIR)/usr/www
 	mkdir -p  $(TARGET_DIR)/usr/www/cgi-bin/
 	cp -rfp $(@D)/src/entry.cgi $(TARGET_DIR)/usr/www/cgi-bin/
 endef
-endif
 
 $(eval $(cmake-package))
