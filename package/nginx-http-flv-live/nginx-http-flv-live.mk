2ddf9c33e04b85bbc5801900fb41df92a2d48ae6
Zhichao Yu <zhichao.yu@rock-chips.com>
2022-12-08 16:43:37 +0800

    pakcage: nginx-http-flv-live: update to v1.2.10
    Signed-off-by: Zhichao Yu <zhichao.yu@rock-chips.com>
    Change-Id: I8c5af86dc478f26b5dfe04847e62023015aa9642

--- package/nginx-http-flv-live/nginx-http-flv-live.mk
+++ package/nginx-http-flv-live/nginx-http-flv-live.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NGINX_HTTP_FLV_LIVE_VERSION = v1.2.8
+NGINX_HTTP_FLV_LIVE_VERSION = v1.2.10
 NGINX_HTTP_FLV_LIVE_SITE = $(call github,winshining,nginx-http-flv-module,$(NGINX_HTTP_FLV_LIVE_VERSION))
 
 $(eval $(generic-package))
