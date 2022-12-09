9127f585a8fecb2e855e3411efe73b4b06d3665c
hangyu.li <hangyu.li@rock-chips.com>
2022-12-09 12:15:09 +0800

    lvgl: update version to 8.2.0
    1. Buildroot adds lvgl configuration file selection;
    2. lvgl supports vector fonts;
    
    Change-Id: I74148bfc1a4de2441c0fbf67d726678f7ba722a3
    Signed-off-by: hangyu.li <hangyu.li@rock-chips.com>

--- package/rockchip/lvgl/lvgl.mk
+++ package/rockchip/lvgl/lvgl.mk
@@ -9,4 +9,16 @@ LVGL_SITE_METHOD = local
 LVGL_SITE = $(TOPDIR)/../external/lvgl
 LVGL_INSTALL_STAGING = YES
 
+LVGL_DEPENDENCIES += freetype
+LVGL_APP_CFLAGS = -I $(STAGING_DIR)/usr/include/freetype2
+
+define LVGL_PRE_RSYNC_INSTALL_CONFIG
+        cp -rfp  $(LVGL_SITE)/rockchip-conf/$(BR2_PACKAGE_LVGL_CONF)  $(LVGL_SITE)/lv_conf.h | true
+endef
+
+LVGL_CONF_OPTS += -DCMAKE_C_FLAGS="$(LVGL_APP_CFLAGS)" \
+                  -DCMAKE_CXX_FLAGS="$(LVGL_APP_CFLAGS)"
+
+LVGL_PRE_RSYNC_HOOKS += LVGL_PRE_RSYNC_INSTALL_CONFIG
+
 $(eval $(cmake-package))
