72728286ab2588761197cc5445b43755e00dac4d
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-12-16 16:16:47 +0800

    nginx-rtmp: Update to v1.2.2
    Change-Id: Ib59b2c3af99745d9615e2793c134e64308f774ef
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/nginx-rtmp/nginx-rtmp.mk
+++ package/nginx-rtmp/nginx-rtmp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NGINX_RTMP_VERSION = v1.2.1
+NGINX_RTMP_VERSION = v1.2.2
 NGINX_RTMP_SITE = $(call github,arut,nginx-rtmp-module,$(NGINX_RTMP_VERSION))
 
 $(eval $(generic-package))
