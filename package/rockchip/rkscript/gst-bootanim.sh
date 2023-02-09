2005119257f6c685f7543fbd2a4cac579a4234c7
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-02-09 17:34:16 +0800

    rkscript: bootanim: Update example animation script
    Change-Id: Iad1222668a0ff3828733a39ecae2ae98a9147667
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/rockchip/rkscript/gst-bootanim.sh
+++ package/rockchip/rkscript/gst-bootanim.sh
@@ -9,4 +9,7 @@ VSINK_ARGS="kmssink force-modesetting=true"
 # Uncomment for fullscreen
 # VSINK_ARGS="$VSINK_ARGS fullscreen=true"
 
-gst-play-1.0 $VIDEO -q --no-interactive --videosink="$VSINK_ARGS"
+gst-play-1.0 $VIDEO -q --no-interactive --videosink="$VSINK_ARGS"&
+
+# Let the caller know that we are started
+touch $TAG_FILE
