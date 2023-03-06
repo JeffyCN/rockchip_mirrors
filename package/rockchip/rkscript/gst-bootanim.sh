a44400a358a5d23718a9daa76b79fe70015575b2
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-03-06 12:23:39 +0800

    rkscript: bootanim: Update example script
    Major changes:
    1/ Disable audio output
    2/ Comment about how to use overlay plane
    
    Change-Id: Iec75d6ebdfa9b26dab22aa295d1a0b2a3be0c9e6
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/rockchip/rkscript/gst-bootanim.sh
+++ package/rockchip/rkscript/gst-bootanim.sh
@@ -4,12 +4,16 @@ VIDEO=$(ls /etc/bootanim.d/*.mp4 2>/dev/null)
 
 [ -z "$VIDEO" ] && exit
 
-VSINK_ARGS="kmssink force-modesetting=true"
+VSINK_ARGS=kmssink
 
-# Uncomment for fullscreen
+# Comment this for using overlay plane
+VSINK_ARGS="$VSINK_ARGS force-modesetting=true"
+
+# Uncomment this for fullscreen
 # VSINK_ARGS="$VSINK_ARGS fullscreen=true"
 
-gst-play-1.0 $VIDEO -q --no-interactive --videosink="$VSINK_ARGS"&
+gst-play-1.0 $VIDEO -q --no-interactive --audiosink=fakesink \
+	--videosink="$VSINK_ARGS"&
 
 # Let the caller know that we are started
-touch $TAG_FILE
+touch ${TAG_FILE:-/dev/null}
