fce2b877d88fa689d4012d891fd4b200f8ad2240
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-04-12 16:10:02 +0800

    cmake: Bump to buildroot upstream's 3.22.3
    Bump to:
    29d6f319a0 utils/check-package: check files in board
    
    Change-Id: I7b6fba64726802c163da962749a1b393bc0380af
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/pkg-cmake.mk
+++ package/pkg-cmake.mk
@@ -51,11 +51,7 @@ endif
 
 define inner-cmake-package
 
-$(2)_CONF_ENV			?=
-$(2)_CONF_OPTS			?=
 $(2)_MAKE			?= $$(MAKE)
-$(2)_MAKE_ENV			?=
-$(2)_MAKE_OPTS			?=
 $(2)_INSTALL_OPTS		?= install
 $(2)_INSTALL_STAGING_OPTS	?= DESTDIR=$$(STAGING_DIR) install/fast
 $(2)_INSTALL_TARGET_OPTS	?= DESTDIR=$$(TARGET_DIR) install/fast
@@ -147,8 +143,10 @@ define $(2)_CONFIGURE_CMDS
 		> $$($$(PKG)_BUILDDIR)/toolchainfile.cmake && \
 	PATH=$$(BR_PATH) \
 	$$($$(PKG)_CONF_ENV) $$(BR2_CMAKE) $$($$(PKG)_SRCDIR) \
+		-G"Unix Makefiles" \
 		-DCMAKE_TOOLCHAIN_FILE="$$($$(PKG)_BUILDDIR)/toolchainfile.cmake" \
 		-DCMAKE_INSTALL_PREFIX="/usr" \
+		-DCMAKE_INSTALL_RUNSTATEDIR="/run" \
 		-DCMAKE_COLOR_MAKEFILE=OFF \
 		-DBUILD_DOC=OFF \
 		-DBUILD_DOCS=OFF \
@@ -176,6 +174,7 @@ define $(2)_CONFIGURE_CMDS
 	PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 \
 	PKG_CONFIG_ALLOW_SYSTEM_LIBS=1 \
 	$$($$(PKG)_CONF_ENV) $$(BR2_CMAKE) $$($$(PKG)_SRCDIR) \
+		-G"Unix Makefiles" \
 		-DCMAKE_INSTALL_SO_NO_EXE=0 \
 		-DCMAKE_FIND_ROOT_PATH="$$(HOST_DIR)" \
 		-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM="BOTH" \
