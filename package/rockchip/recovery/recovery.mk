c4286dc072e5c137ecee144bc02e0f64a8100ed8
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-04-23 18:23:51 +0800

    board: rockchip: Drop recovery overlay
    Install S40recovery in recovery package instead.
    
    Change-Id: I71193aa2360e46494379688953ff82bbd400b3d3
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/rockchip/recovery/recovery.mk
+++ package/rockchip/recovery/recovery.mk
@@ -24,26 +24,26 @@ RECOVERY_BUILD_OPTS+=-I$(PROJECT_DIR) -I$(STAGING_DIR)/usr/include/libdrm \
 RECOVERY_DEPENDENCIES += libcurl openssl
 
 ifeq ($(BR2_PACKAGE_RECOVERY_NO_UI),y)
-	TARGET_MAKE_ENV += RecoveryNoUi=true
+TARGET_MAKE_ENV += RecoveryNoUi=true
 else
-	RECOVERY_BUILD_OPTS += -lz -lpng -ldrm
-	RECOVERY_DEPENDENCIES += libzlib libpng libdrm
+RECOVERY_BUILD_OPTS += -lz -lpng -ldrm
+RECOVERY_DEPENDENCIES += libzlib libpng libdrm
 endif
 
 ifeq ($(BR2_PACKAGE_RECOVERY_USE_RKUPDATE),y)
-	TARGET_CFLAGS += -DUSE_RKUPDATE=ON
+TARGET_CFLAGS += -DUSE_RKUPDATE=ON
 endif
 
 ifeq ($(BR2_PACKAGE_RECOVERY_USE_UPDATEENGINE),y)
-	TARGET_CFLAGS += -DUSE_UPDATEENGINE=ON
+TARGET_CFLAGS += -DUSE_UPDATEENGINE=ON
 endif
 
 ifeq ($(BR2_PACKAGE_RECOVERY_SUCCESSFUL_BOOT),y)
-	TARGET_CFLAGS += -DSUCCESSFUL_BOOT=ON
+TARGET_CFLAGS += -DSUCCESSFUL_BOOT=ON
 endif
 
 ifeq ($(BR2_PACKAGE_RECOVERY_RETRY),y)
-	TARGET_CFLAGS += -DRETRY_BOOT=ON
+TARGET_CFLAGS += -DRETRY_BOOT=ON
 endif
 
 RECOVERY_MAKE_OPTS = \
@@ -62,6 +62,11 @@ endef
 ifeq ($(BR2_PACKAGE_RECOVERY_RECOVERYBIN),y)
 define RECOVERYBIN_INSTALL_TARGET
 	$(INSTALL) -D -m 755 $(@D)/recovery $(TARGET_DIR)/usr/bin/ && $(RECOVERY_IMAGE_COPY)
+endef
+
+define RECOVERY_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 $(RECOVERY_PKGDIR)/S40recovery \
+		$(TARGET_DIR)/etc/init.d/S40recovery
 endef
 endif
 
@@ -82,17 +87,5 @@ define RECOVERY_INSTALL_TARGET_CMDS
 	$(UPDATEENGINEBIN_INSTALL_TARGET)
 	$(BOOTCONTROLBIN_INSTALL_TARGET)
 endef
-
-ifeq ($(BR2_PACKAGE_RECOVERY_RECOVERYBIN),y)
-define RECOVERY_IMAGE_PACK
-	mkdir -p $(TARGET_DIR)/res/images
-	cp $(BUILD_DIR)/recovery-$(RECOVERY_VERSION)/res/images/* $(TARGET_DIR)/res/images/
-	$(HOST_DIR)/bin/mkbootfs $(TARGET_DIR) | $(HOST_DIR)/bin/minigzip > $(BINARIES_DIR)/ramdisk-recovery.img
-	$(RECOVERY_MKBOOTIMG) --kernel $(RECOVERY_MK_KERNEL_IMAGE) --ramdisk $(BINARIES_DIR)/ramdisk-recovery.img --second $(RECOVERY_RESOURCEIMG) --os_version $(RECOVERY_OS_VERSION) --os_patch_level $(RECOVERY_OS_PATCH_LEVEL) --cmdline $(RECOVERY_CMDLINNE) --output $(BINARIES_DIR)/recovery.img
-	$(RECOVERY_MK_KERNEL_IMAGE) $(BINARIES_DIR)/ramdisk-recovery.img $(BINARIES_DIR)/recovery.img
-endef
-
-#TARGET_FINALIZE_HOOKS += RECOVERY_IMAGE_PACK
-endif
 
 $(eval $(generic-package))
