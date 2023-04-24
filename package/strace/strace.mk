8b51ff216091afee2f5942376ffdefeb2402efbf
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-04-24 18:02:58 +0800

    strace: Support static
    Set BR2_PACKAGE_STRACE_STATIC to enable.
    
    Change-Id: If8f82fe48c21098d01a7822eb218024b5ac21150
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/strace/strace.mk
+++ package/strace/strace.mk
@@ -27,6 +27,11 @@ else
 STRACE_CONF_OPTS += --without-libiberty
 endif
 
+ifeq ($(BR2_PACKAGE_STRACE_STATIC),y)
+STRACE_CONF_OPTS += --enable-static
+STRACE_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -static"
+endif
+
 ifeq ($(BR2_PACKAGE_PERL),)
 define STRACE_REMOVE_STRACE_GRAPH
 	rm -f $(TARGET_DIR)/usr/bin/strace-graph
