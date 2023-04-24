d2e9319ceb34a1aa2dc68aa570d3179c1c3252cb
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-04-24 18:02:58 +0800

    libunwind: Support static
    Set BR2_PACKAGE_LIBUNWIND_STATIC to enable.
    
    Change-Id: I1a9ecb9cdbf7637c35db2f7cd8b4840dffd08415
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/libunwind/libunwind.mk
+++ package/libunwind/libunwind.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUNWIND_VERSION = 1.5.0
+LIBUNWIND_VERSION = 1.6.2
 LIBUNWIND_SITE = http://download.savannah.gnu.org/releases/libunwind
 LIBUNWIND_INSTALL_STAGING = YES
 LIBUNWIND_LICENSE_FILES = COPYING
@@ -16,8 +16,8 @@ LIBUNWIND_CONF_OPTS = \
 	--disable-tests \
 	$(if $(BR2_INSTALL_LIBSTDCPP),--enable-cxx-exceptions,--disable-cxx-exceptions)
 
-ifeq ($(BR2_PACKAGE_LIBATOMIC_OPS),y)
-LIBUNWIND_DEPENDENCIES += libatomic_ops
+ifeq ($(BR2_PACKAGE_LIBUNWIND_STATIC),y)
+LIBUNWIND_CONF_OPTS += --enable-static
 endif
 
 $(eval $(autotools-package))
