b9935748eac63825b6be52442281d0275f442b04
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-05-04 10:10:24 +0800

    Add async-commit
    Enable ASYNC_COMMIT for Rockchip BSP kernel > 4.4.
    
    Change-Id: I0f95e213471e8a054b59d6b2811bc260806ec2c7
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

---
+++ S10async-commit.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:       async-commit
+# Default-Start:  S
+# Default-Stop:
+# Description:    Enable ASYNC_COMMIT for Rockchip BSP kernel > 4.4
+### END INIT INFO
+
+case "$1" in
+	start)
+		/usr/bin/async-commit ;;
+	stop)
+		;;
+	restart|reload)
+		;;
+	*)
+		echo "Usage: $0 {start|stop|restart}"
+		exit 1
+esac
+
+exit $?
