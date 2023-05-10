13fe4e42ff29b2c63489a20d0405a4d504030432
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-05-10 12:19:09 +0800

    rknpu2: Use buildroot compiler
    Change-Id: Ic81c475d488baa39e4ed0f86d4242aead8d97dd2
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/rockchip/rknpu2/rknpu2.mk
+++ package/rockchip/rknpu2/rknpu2.mk
@@ -11,41 +11,38 @@ RKNPU2_INSTALL_STAGING = YES
 RKNPU2_LICENSE = ROCKCHIP
 RKNPU2_LICENSE_FILES = LICENSE
 
-ifeq ($(BR2_arm),y)
-GCC_TYPE=arm-none-linux-gnueabihf
-EXPORT_PATH=$(PATH):$(TOPDIR)/../prebuilts/gcc/linux-x86/arm/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf/bin
-NPU_PLATFORM_ARCH = armhf
-else
-GCC_TYPE=aarch64-none-linux-gnu
-EXPORT_PATH=$(PATH):$(TOPDIR)/../prebuilts/gcc/linux-x86/aarch64/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin
-NPU_PLATFORM_ARCH = aarch64
-endif
+RKNPU2_ARCH = $(call qstrip,$(BR2_PACKAGE_RKNPU2_ARCH))
+RKNPU2_SOC = $(call qstrip,$(BR2_PACKAGE_RKNPU2_SOC))
 
-ifneq ($(BR2_PACKAGE_RK3566_RK3568)$(BR2_PACKAGE_RK3562),)
-NPU_PLATFORM_INFO = RK356X
-NPU_DEMO_BUILD = build-linux_RK356X.sh
-endif
-
-ifeq ($(BR2_PACKAGE_RK3588), y)
-NPU_PLATFORM_INFO = RK3588
-NPU_DEMO_BUILD = build-linux_RK3588.sh
-endif
-
-define RKNPU2_BUILD_CMDS
-	cd $(@D)/examples/rknn_common_test && export PATH=$(EXPORT_PATH) && export GCC_COMPILER=$(GCC_TYPE) && ./$(NPU_DEMO_BUILD)
+define RKNPU2_INSTALL_TARGET_CMDS
+	cp -r $(@D)/runtime/$(RKNPU2_SOC)/Linux/rknn_server/$(RKNPU2_ARCH)/* \
+		$(TARGET_DIR)/
+	cp -r $(@D)/runtime/$(RKNPU2_SOC)/Linux/librknn_api/$(RKNPU2_ARCH)/* \
+		$(TARGET_DIR)/usr/lib/
 endef
 
 define RKNPU2_INSTALL_STAGING_CMDS
-	mkdir -p $(STAGING_DIR)/usr/include/rknn
-	$(INSTALL) -D -m 0644 $(@D)/runtime/$(NPU_PLATFORM_INFO)/Linux/librknn_api/include/rknn_api.h ${TARGET_DIR}/usr/include/rknn/rknn_api.h
+	cp -r $(@D)/runtime/$(RKNPU2_SOC)/Linux/librknn_api/$(RKNPU2_ARCH)/* \
+		$(STAGING_DIR)/usr/lib/
+	cp -rT $(@D)/runtime/$(RKNPU2_SOC)/Linux/librknn_api/include \
+		$(STAGING_DIR)/usr/include/rknn
 endef
 
-define RKNPU2_INSTALL_TARGET_CMDS
-	cp -r $(@D)/runtime/$(NPU_PLATFORM_INFO)/Linux/rknn_server/${NPU_PLATFORM_ARCH}/usr/bin/* ${TARGET_DIR}/usr/bin/
-	cp -r $(@D)/runtime/$(NPU_PLATFORM_INFO)/Linux/librknn_api/${NPU_PLATFORM_ARCH}/* ${TARGET_DIR}/usr/lib/
-	$(INSTALL) -D -m 0755 $(@D)/examples/rknn_common_test/install/rknn_common_test_Linux/rknn_common_test ${TARGET_DIR}/usr/bin/
-	cp -r $(@D)/examples/rknn_common_test/install/rknn_common_test_Linux/lib/* ${TARGET_DIR}/usr/lib/
-	cp -r $(@D)/examples/rknn_common_test/install/rknn_common_test_Linux/model/ ${TARGET_DIR}/usr/share/
-endef
-
+ifeq ($(BR2_PACKAGE_RKNPU2_EXAMPLE),)
 $(eval $(generic-package))
+else
+RKNPU2_SUBDIR = examples/rknn_common_test
+RKNPU2_CONF_OPTS += -DTARGET_SOC=$(call LOWERCASE,$(RKNPU2_SOC))
+
+define RKNPU2_INSTALL_TARGET_EXAMPLE
+	cp -r $(@D)/runtime/$(RKNPU2_SOC)/Linux/librknn_api/$(RKNPU2_ARCH)/* \
+		$(STAGING_DIR)/usr/lib/
+	cp -rT $(@D)/runtime/$(RKNPU2_SOC)/Linux/librknn_api/include \
+		$(STAGING_DIR)/usr/include/rknn
+
+	cp $(@D)/$(RKNPU2_SUBDIR)/rknn_common_test $(TARGET_DIR)/usr/bin/
+	cp -r $(@D)/$(RKNPU2_SUBDIR)/model $(TARGET_DIR)/usr/share/
+endef
+RKNPU2_POST_INSTALL_TARGET_HOOKS += RKNPU2_INSTALL_TARGET_EXAMPLE
+$(eval $(cmake-package))
+endif
