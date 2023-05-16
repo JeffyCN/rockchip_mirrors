eaa0c0792aa02f0a7e4341557918608c9391ad33
Jianlong Wang <jianlong.wang@rock-chips.com>
2023-05-16 09:09:51 +0800

    package: rockchip: rockchip-test: remove redundant actions
    Change-Id: Ia2b72488f00b5cc6a59b460afc3fca2dcc31b748
    Signed-off-by: Jianlong Wang <jianlong.wang@rock-chips.com>

--- package/rockchip/rockchip-test/rockchip-test.mk
+++ package/rockchip/rockchip-test/rockchip-test.mk
@@ -9,7 +9,6 @@ ROCKCHIP_TEST_LICENSE_FILES = LICENSE
 define ROCKCHIP_TEST_INSTALL_TARGET_CMDS
 	mkdir -p ${TARGET_DIR}/rockchip-test
 	cp -rf  $(@D)/*  ${TARGET_DIR}/rockchip-test/
-	$(INSTALL) -D -m 0755 $(@D)/auto_reboot/S99-auto-reboot $(TARGET_DIR)/etc/init.d/
 endef
 
 define ROCKCHIP_TEST_INSTALL_INIT_SYSV
