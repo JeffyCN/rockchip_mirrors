9ffb292c55570bb2d24c89a8717b609e9b3e7658
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-07-18 14:15:23 +0800

    download: Support pre-downloaded archives
    Prefer using pre-downloaded archives under archives/.
    
    Change-Id: I58146ea864a1ed47bd30263b46914231d6753094
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/pkg-download.mk
+++ package/pkg-download.mk
@@ -111,7 +111,7 @@ define DOWNLOAD
 		flock $($(2)_DL_DIR)/.lock $(DL_WRAPPER) \
 		-c '$($(2)_DL_VERSION)' \
 		-d '$($(2)_DL_DIR)' \
-		-D '$(DL_DIR)' \
+		-D '$(TOPDIR)/archives' \
 		-f '$(notdir $(1))' \
 		-H '$($(2)_HASH_FILE)' \
 		-n '$($(2)_BASENAME_RAW)' \
