d6c156a9a01593a20adb3901d7309a6c7b2e8d42
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-08-25 18:22:59 +0800

    fs/ext2: Refactor shrinking flow
    Major changes:
    1/ Shrinking image before compressing it.
    2/ Calculating rootfs size dynamically.
    
    Change-Id: I9869fdb657bda6e764fa0d7dd11a27596863c1fa
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- fs/ext2/ext2.mk
+++ fs/ext2/ext2.mk
@@ -9,19 +9,6 @@ ifeq ($(BR2_TARGET_ROOTFS_EXT2)-$(ROOTFS_EXT2_SIZE),y-)
 $(error BR2_TARGET_ROOTFS_EXT2_SIZE cannot be empty)
 endif
 
-# If SIZE is AUTO, which mean shrink the filesystem to the minimum size.
-# Suppose 2G is big enough for buildroot ext filesystem, we then shrink
-# it after rootfs img packed.
-ifeq ($(ROOTFS_EXT2_SIZE),AUTO)
-ROOTFS_EXT2_SIZE = 2G
-define ROOTFS_EXT2_SHRINK
-	$(HOST_DIR)/sbin/resize2fs -M $(BINARIES_DIR)/rootfs.ext2
-	$(HOST_DIR)/sbin/e2fsck -fy $(BINARIES_DIR)/rootfs.ext2
-	$(HOST_DIR)/sbin/tune2fs -m $(BR2_TARGET_ROOTFS_EXT2_RESBLKS) $(BINARIES_DIR)/rootfs.ext2
-	$(HOST_DIR)/sbin/resize2fs -M $(BINARIES_DIR)/rootfs.ext2
-endef
-endif
-
 ROOTFS_EXT2_MKFS_OPTS = $(call qstrip,$(BR2_TARGET_ROOTFS_EXT2_MKFS_OPTIONS))
 
 # qstrip results in stripping consecutive spaces into a single one. So the
@@ -39,6 +26,7 @@ ROOTFS_EXT2_OPTS = \
 
 ROOTFS_EXT2_DEPENDENCIES = host-e2fsprogs
 
+ifneq ($(ROOTFS_EXT2_SIZE),auto)
 define ROOTFS_EXT2_CMD
 	rm -f $@
 	$(HOST_DIR)/sbin/mkfs.ext$(BR2_TARGET_ROOTFS_EXT2_GEN) $(ROOTFS_EXT2_OPTS) $@ \
@@ -48,12 +36,24 @@ define ROOTFS_EXT2_CMD
 	     exit $$ret; \
 	}
 endef
+else
+define ROOTFS_EXT2_CMD
+	rm -f $@
+	ROOTFS_SIZE="$$(du -shm $(TARGET_DIR) | cut -f1)"
+	$(HOST_DIR)/sbin/mkfs.ext$(BR2_TARGET_ROOTFS_EXT2_GEN) \
+		$(ROOTFS_EXT2_OPTS) \
+		$@ "$$(($$ROOTFS_SIZE + \
+		$$ROOTFS_SIZE * (10 + $(BR2_TARGET_ROOTFS_EXT2_RESBLKS)) / 100))M"
+	$(HOST_DIR)/sbin/resize2fs -M $@
+	$(HOST_DIR)/sbin/e2fsck -fy $@
+endef
+endif
 
 ifneq ($(BR2_TARGET_ROOTFS_EXT2_GEN),2)
 define ROOTFS_EXT2_SYMLINK
 	ln -sf rootfs.ext2$(ROOTFS_EXT2_COMPRESS_EXT) $(BINARIES_DIR)/rootfs.ext$(BR2_TARGET_ROOTFS_EXT2_GEN)$(ROOTFS_EXT2_COMPRESS_EXT)
 endef
-ROOTFS_EXT2_POST_GEN_HOOKS += ROOTFS_EXT2_SYMLINK ROOTFS_EXT2_SHRINK
+ROOTFS_EXT2_POST_GEN_HOOKS += ROOTFS_EXT2_SYMLINK
 endif
 
 $(eval $(rootfs))
