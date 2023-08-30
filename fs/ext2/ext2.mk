91eddcc8ae2ebc8c14c03d3cf1e6b9421ca614c6
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-08-30 09:37:41 +0800

    fs: ext2: Increase 50M freespace for auto size with small rootfs
    Tested on RK3588 recovery.
    
    Change-Id: Ice641b597cd1a1fbdae1f3bac89d0f7ad359a316
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- fs/ext2/ext2.mk
+++ fs/ext2/ext2.mk
@@ -42,8 +42,8 @@ define ROOTFS_EXT2_CMD
 	ROOTFS_SIZE="$$(du -shm $(TARGET_DIR) | cut -f1)"
 	$(HOST_DIR)/sbin/mkfs.ext$(BR2_TARGET_ROOTFS_EXT2_GEN) \
 		$(ROOTFS_EXT2_OPTS) \
-		$@ "$$(($$ROOTFS_SIZE + \
-		$$ROOTFS_SIZE * (10 + $(BR2_TARGET_ROOTFS_EXT2_RESBLKS)) / 100))M"
+		$@ "$$(($$ROOTFS_SIZE + 50 + \
+		$$ROOTFS_SIZE * (8 + $(BR2_TARGET_ROOTFS_EXT2_RESBLKS)) / 100))M"
 	$(HOST_DIR)/sbin/resize2fs -M $@
 	$(HOST_DIR)/sbin/e2fsck -fy $@
 endef
