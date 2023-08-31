65630ce2d1e1c8fd85aaa0958250d048f6c622d3
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-08-31 14:42:58 +0800

    Rename S50usbdevice to S50usbdevice.sh
    Keep S50usbdevice linked for old build scripts.
    
    Change-Id: Icf3a996349fd674705aa781a5fa5458f30b6001b
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

---
+++ S50usbdevice.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:       usbdevice
+# Required-Start: $local_fs $syslog
+# Required-Stop:  $local_fs
+# Default-Start:  S
+# Default-Stop:   K
+# Description:    Manage USB device functions
+### END INIT INFO
+
+case "$1" in
+	start|stop|restart)
+		/usr/bin/usbdevice $1
+		;;
+	*)
+		echo "Usage: [start|stop|restart]" >&2
+		exit 3
+		;;
+esac
+
+:
