b38295baada147c9788be90656d61513dbb83c53
ZiHan Huang <zack.huang@rock-chips.com>
2023-09-01 10:16:47 +0800

    package/rockchip/rockit: Add chip segmentation
    Change-Id: I3df859c38116bc05a3f76ffa15009d3de2101e18
    Signed-off-by: ZiHan Huang <zack.huang@rock-chips.com>

--- package/rockchip/rockit/rockit.mk
+++ package/rockchip/rockit/rockit.mk
@@ -10,4 +10,12 @@ ROCKIT_SITE_METHOD = local
 
 ROCKIT_INSTALL_STAGING = YES
 
+ifneq ($(BR2_PACKAGE_RK3308),)
+ROCKIT_CONF_OPTS += -DRK3308=TRUE
+endif
+
+ifneq ($(BR2_PACKAGE_RK3506),)
+ROCKIT_CONF_OPTS += -DRK3506=TRUE
+endif
+
 $(eval $(cmake-package))
