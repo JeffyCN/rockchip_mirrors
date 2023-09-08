572dafe06f5ed1068d3deb9988e8b1984ad004c3
Jianlong Wang <jianlong.wang@rock-chips.com>
2023-09-08 11:34:50 +0800

    rockchip-mpp: Remove unused rk3328 h265e firmware
    The rk3328 h265 encoding is no longer supported in kernel.
    
    Change-Id: I4883d5c03881d49fbb0894dad4b450e31f27d0b7
    Signed-off-by: Jianlong Wang <jianlong.wang@rock-chips.com>
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/rockchip/rockchip-mpp/rockchip-mpp.mk
+++ package/rockchip/rockchip-mpp/rockchip-mpp.mk
@@ -24,21 +24,6 @@ ifeq ($(BR2_PACKAGE_ROCKCHIP_MPP_TESTS),y)
 ROCKCHIP_MPP_CONF_OPTS += "-DBUILD_TEST=ON"
 endif
 
-ifeq ($(BR2_PACKAGE_RK3328),y)
-define ROCKCHIP_MPP_H265_SUPPORTED_FIRMWARE
-	mkdir -p $(TARGET_DIR)/lib/firmware/
-
-	if test -e $(ROCKCHIP_MPP_SITE)/../rktoolkit/monet.bin ; then \
-		$(INSTALL) -m 0644 -D $(ROCKCHIP_MPP_SITE)/../rktoolkit/monet.bin \
-			$(TARGET_DIR)/lib/firmware/ ; \
-	else \
-		$(INSTALL) -m 0644 -D package/rockchip/rockchip-mpp/monet.bin \
-			$(TARGET_DIR)/lib/firmware/ ; \
-	fi
-endef
-ROCKCHIP_MPP_POST_INSTALL_TARGET_HOOKS += ROCKCHIP_MPP_H265_SUPPORTED_FIRMWARE
-endif
-
 define ROCKCHIP_MPP_REMOVE_NOISY_LOGS
 	sed -i -e "/pp_enable %d/d" \
 		$(@D)/mpp/hal/vpu/jpegd/hal_jpegd_vdpu2.c || true
