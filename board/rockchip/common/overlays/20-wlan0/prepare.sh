dc239b7f558d12f5e3cb43f97d592e27fd91cdfa
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-09-25 14:37:08 +0800

    board: rockchip: Refactor overlays a bit
    Drop custom install scripts.
    
    Tested with:
    ./board/rockchip/common/post-build.sh output/rockchip_rk3588/target/
    
    Change-Id: I0830b8edfe360d4fa3dfb02cdac9708e63e53265
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- board/rockchip/common/overlays/20-wlan0/prepare.sh
+++ board/rockchip/common/overlays/20-wlan0/prepare.sh
@@ -4,3 +4,8 @@
 [ -x "$TARGET_DIR/sbin/dhcpcd" ]
 [ -r "$TARGET_DIR/etc/network/interfaces" ]
 [ -r "$TARGET_DIR/etc/wpa_supplicant.conf" ]
+
+if ! grep -wq "interfaces.d" "$TARGET_DIR/etc/network/interfaces"; then
+	echo -e "\nsource-directory /etc/network/interfaces.d" >> \
+		"$TARGET_DIR/etc/network/interfaces"
+fi
