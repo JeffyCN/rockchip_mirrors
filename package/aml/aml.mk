de15e387fe1083ab87b6cab899ba24c3750ef7f5
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-10-16 15:26:30 +0800

    aml: Bump to 0.3.0
    Change-Id: Id98dd41ed3d9b3c3d483cb66ff9bed27a9e70e5a
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/aml/aml.mk
+++ package/aml/aml.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 AML_SITE = https://github.com/any1/aml.git
-AML_VERSION = 3285a4345deaf6ae63a88cb666ecd743090b0081
+AML_VERSION = v0.3.0
 AML_SITE_METHOD = git
 AML_LICENSE = ISC
 AML_LICENSE_FILES = COPYING
