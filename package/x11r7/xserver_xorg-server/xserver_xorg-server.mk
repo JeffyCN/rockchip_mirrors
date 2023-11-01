a6543e0831ee423099c9a84f5e4689ec84ecd7d4
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-11-01 11:03:53 +0800

    xorg-xserver: Bump to 21.1.8_2023_10_26
    Change-Id: Ie6ef0a9d847673651756df243aabd55ed92009af
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/x11r7/xserver_xorg-server/xserver_xorg-server.mk
+++ package/x11r7/xserver_xorg-server/xserver_xorg-server.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XSERVER_XORG_SERVER_VERSION = 21.1.4_2023_04_07
+XSERVER_XORG_SERVER_VERSION = 21.1.8_2023_10_26
 XSERVER_XORG_SERVER_SITE = $(call github,JeffyCN,xorg-xserver,$(XSERVER_XORG_SERVER_VERSION))
 XSERVER_XORG_SERVER_LICENSE = MIT
 XSERVER_XORG_SERVER_LICENSE_FILES = COPYING
