7e0f7d7f4616626b5d11e53fb13ec53a9a3c5e64
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-01-22 17:24:38 +0800

    weston: Improve weston-touch-calibrator for multi inputs
    Create separate config files for different input devices.
    
    Change-Id: I7c3e9b6ac0dc14824799a01eb775575e649abd57
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/weston/weston-calibration-helper.sh
+++ package/weston/weston-calibration-helper.sh
@@ -1,6 +1,21 @@
-#!/bin/bash -x
+#!/bin/sh -e
 
-cat <<EOF >/etc/profile.d/weston-calibration.sh
-# Calibration for $1
-export WESTON_TOUCH_CALIBRATION="$2 $3 $4 $5 $6 $7"
+SYSPATH="$1"
+SYSNAME="$(basename "$SYSPATH")"
+
+DEVNAME_PATH="$SYSPATH/device/name"
+if [ -r "$DEVNAME_PATH" ]; then
+	NAME="$(cat "$DEVNAME_PATH")"
+else
+	NAME="$SYSNAME"
+fi
+
+OUTPUT="/etc/profile.d/weston-calibration-$SYSNAME.sh"
+cat <<EOF >"$OUTPUT"
+# Calibration for $SYSPATH
+export WESTON_TOUCH_CALIBRATION="\$(echo \$WESTON_TOUCH_CALIBRATION | tr ',' '\n' | grep -v "^$NAME" || true)"
+export WESTON_TOUCH_CALIBRATION="$NAME:$2 $3 $4 $5 $6 $7,\$WESTON_TOUCH_CALIBRATION"
 EOF
+
+echo "Generated: $OUTPUT"
+cat "$OUTPUT"
