facaa5bfa0e77f95cf99b569e22354746a93a262
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-03-04 18:20:16 +0800

    rockchip-rga: Workaround conflict with GCC's fixincl
    Change-Id: I6b7ed12eb29c4d9024642a053a9f6f5626d5d768
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/rockchip/rockchip-rga/rockchip-rga.mk
+++ package/rockchip/rockchip-rga/rockchip-rga.mk
@@ -13,6 +13,13 @@ ROCKCHIP_RGA_LICENSE_FILES = COPYING
 
 ROCKCHIP_RGA_INSTALL_STAGING = YES
 
+# Avoid conflict with GCC's fixincl
+define ROCKCHIP_RGA_FIX_INCLUDE
+	$(SED) 's/ linux / __linux__ /' $(@D)/include/RgaApi.h \
+		2>/dev/null || true
+endef
+ROCKCHIP_RGA_POST_RSYNC_HOOKS += ROCKCHIP_RGA_FIX_INCLUDE
+
 ifeq ($(BR2_PACKAGE_LIBDRM),y)
 ROCKCHIP_RGA_DEPENDENCIES += libdrm
 ROCKCHIP_RGA_CONF_OPTS += -Dlibdrm=true
