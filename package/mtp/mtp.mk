a1b50721b0acdd559cb3369d947831b4ea7db132
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-03-08 12:06:44 +0800

    mtp: New package
    Change-Id: If55cdbb1e8373c270b9f12532117177100515790
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

---
+++ package/mtp/mtp.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# mtp
+#
+################################################################################
+
+MTP_VERSION = 71
+MTP_SITE = https://launchpad.net/mtp
+MTP_SITE_METHOD = bzr
+MTP_INSTALL_STAGING = YES
+
+MTP_LICENSE = Apache-2.0
+MTP_LICENSE_FILES = NOTICE
+
+MTP_DEPENDENCIES = boost dbus-cpp glog
+
+define MTP_ADD_PTHREAD_TO_CMAKE_CXX_FLAGS
+	$(SED) 's%-std=c++11%-std=c++11 -lpthread%' $(@D)/CMakeLists.txt
+endef
+MTP_POST_PATCH_HOOKS += MTP_ADD_PTHREAD_TO_CMAKE_CXX_FLAGS
+
+$(eval $(cmake-package))
