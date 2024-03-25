5a600310a4183d111a5629f51148218ba5c8a45d
LiuDiMing Lin <fenrir.lin@rock-chips.com>
2024-03-25 10:58:27 +0800

    package: rockchip: add rkipc application
    Change-Id: Ieecc0da3bb566538696476020ec0446864df349c
    Signed-off-by: LiuDiMing Lin <fenrir.lin@rock-chips.com>

---
+++ package/rockchip/ipc/rkipc/rkipc.mk
@@ -0,0 +1,27 @@
+RKIPC_SITE = $(TOPDIR)/../app/rkipc
+RKIPC_SITE_METHOD = local
+RKIPC_LICENSE = ROCKCHIP
+RKIPC_LICENSE_FILES = LICENSE
+
+RKIPC_DEPENDENCIES = camera-engine-rkaiq wpa_supplicant freetype common_algorithm
+
+ifeq ($(BR2_PACKAGE_RKIPC_RK3588), y)
+    RKIPC_DEPENDENCIES += rkfsmk rockit rockchip-rga
+    RKIPC_CONF_OPTS += -DCOMPILE_FOR_RK3588=ON
+endif
+
+ifeq ($(BR2_PACKAGE_RKIPC_RK3588_MULTI_IPC), y)
+    RKIPC_CONF_OPTS += -DCOMPILE_FOR_RK3588_MULTI_IPC=ON
+endif
+
+ifeq ($(BR2_PACKAGE_RKIPC_RV1126), y)
+    RKIPC_DEPENDENCIES += rkmedia rkfsmk
+    RKIPC_CONF_OPTS += -DCOMPILE_FOR_RV1126_RKMEDIA=ON
+endif
+
+ifeq ($(BR2_PACKAGE_RKIPC_RV1126_BATTERY_IPC), y)
+	RKIPC_DEPENDENCIES += rockit rktoolkit
+	RKIPC_CONF_OPTS += -DCOMPILE_FOR_RV1126_BATTERY_IPC=ON
+endif
+
+$(eval $(cmake-package))
