18b97bd0f9df5afc38500d29d09c6cabd746a4c7
LiuDiMing Lin <fenrir.lin@rock-chips.com>
2024-03-28 16:53:26 +0800

    package: rockchip: ipc: add avs
    Change-Id: I27420e6aa777c846014825031a4f401981419b52
    Signed-off-by: LiuDiMing Lin <fenrir.lin@rock-chips.com>

---
+++ package/rockchip/ipc/avs/avs.mk
@@ -0,0 +1,15 @@
+AVS_SITE = $(TOPDIR)/../external/avs
+AVS_SITE_METHOD = local
+
+AVS_INSTALL_STAGING = YES
+
+AVS_CONF_OPTS += -DCONFIG_RK_AVS=ON
+
+define AVS_INSTALL_TARGET_CMDS
+	cp -rfp $(@D)/lib/*.so $(TARGET_DIR)/usr/lib/
+	cp -rfp $(@D)/lib/*.so $(HOST_DIR)/aarch64-buildroot-linux-gnu/sysroot/usr/lib/
+	cp -rfp $(@D)/lib/*.h $(HOST_DIR)/aarch64-buildroot-linux-gnu/sysroot/usr/include/
+	cp -rfp $(@D)/avs_calib/ $(HOST_DIR)/aarch64-buildroot-linux-gnu/sysroot/usr/share/
+endef
+
+$(eval $(generic-package))
