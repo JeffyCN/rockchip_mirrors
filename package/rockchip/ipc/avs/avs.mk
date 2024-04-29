1286007f36ed7aaedfdf55002d3b9680190d29c0
LiuDiMing Lin <fenrir.lin@rock-chips.com>
2024-04-29 11:57:29 +0800

    package: rockchip: ipc: avs: fix the path of the xml file
    Change-Id: Ic88c20bd49635d3975d511e045ae5cb5a060a9bf
    Signed-off-by: LiuDiMing Lin <fenrir.lin@rock-chips.com>

--- package/rockchip/ipc/avs/avs.mk
+++ package/rockchip/ipc/avs/avs.mk
@@ -9,7 +9,7 @@ define AVS_INSTALL_TARGET_CMDS
 	cp -rfp $(@D)/lib/*.so $(TARGET_DIR)/usr/lib/
 	cp -rfp $(@D)/lib/*.so $(HOST_DIR)/aarch64-buildroot-linux-gnu/sysroot/usr/lib/
 	cp -rfp $(@D)/lib/*.h $(HOST_DIR)/aarch64-buildroot-linux-gnu/sysroot/usr/include/
-	cp -rfp $(@D)/avs_calib/ $(HOST_DIR)/aarch64-buildroot-linux-gnu/sysroot/usr/share/
+	cp -rfp $(@D)/avs_calib/ $(TARGET_DIR)/usr/share/
 endef
 
 $(eval $(generic-package))
