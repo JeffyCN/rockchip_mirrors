7dfad3d47dd49ec46e30ac12e6706ba33d213eb1
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-05-09 15:54:59 +0800

    umtprd: Update to newer version
    Major changes:
    1/ Update to:
    7d7d099 unicode2charstring : Use bytes accesses instead of half word accesses to support unaligned addresses on pre ARMv7 CPUs.
    2/ Support forcing static build.
    3/ Use /dev/usb-ffs/mtp/ like adbd.
    4/ Install to the original /usr/bin/ like adbd.
    
    Change-Id: Iada14235d4e8b3e9fef69469944b16e5ec586669
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/umtprd/umtprd.mk
+++ package/umtprd/umtprd.mk
@@ -4,17 +4,37 @@
 #
 ################################################################################
 
-UMTPRD_VERSION = 1.3.10
-UMTPRD_SITE = https://github.com/viveris/uMTP-Responder/archive
+UMTPRD_VERSION = 7d7d099df0dd929f95de0f45a2b6b006545207c6
+UMTPRD_SITE = https://github.com/viveris/uMTP-Responder.git
+UMTPRD_SITE_METHOD = git
 UMTPRD_LICENSE = GPL-3.0+
 UMTPRD_LICENSE_FILES = LICENSE
 
+ifeq ($(BR2_PACKAGE_SYSTEMD),y)
+UMTPRD_DEPENDENCIES = systemd
+UMTPRD_MAKE_OPTS += SYSTEMD=1
+endif
+
+ifeq ($(BR2_PACKAGE_UMTPRD_USE_SYSLOG),y)
+UMTPRD_MAKE_OPTS += USE_SYSLOG=1
+endif
+
+ifeq ($(BR2_PACKAGE_UMTPRD_STATIC),y)
+UMTPRD_MAKE_OPTS += CC="$(TARGET_CC) -static"
+endif
+
+define UMTPRD_FIXUP_USB_PATH
+	sed -i -e "s~/dev/ffs-umtp/~/dev/usb-ffs/mtp/~" \
+		$(@D)/inc/default_cfg.h || true
+endef
+UMTPRD_POST_PATCH_HOOKS += UMTPRD_FIXUP_USB_PATH
+
 define UMTPRD_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) $(UMTPRD_MAKE_OPTS) -C $(@D)
 endef
 
 define UMTPRD_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/umtprd $(TARGET_DIR)/usr/sbin/umtprd
+	$(INSTALL) -D -m 0755 $(@D)/umtprd $(TARGET_DIR)/usr/bin/umtprd
 endef
 
 $(eval $(generic-package))
