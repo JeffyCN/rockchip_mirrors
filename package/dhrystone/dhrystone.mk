34121d44006bfd5bd7a8bced5e8cc7a8e7fb6b04
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-05-30 10:46:11 +0800

    dhrystone: Support forcing static build
    Change-Id: If9f4ba93b3dadfbbdd7a95fb228bc366e3cac467
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/dhrystone/dhrystone.mk
+++ package/dhrystone/dhrystone.mk
@@ -8,13 +8,19 @@ DHRYSTONE_VERSION = 2
 DHRYSTONE_SOURCE = dhry-c
 DHRYSTONE_SITE = http://www.netlib.org/benchmark
 
+DHRYSTONE_CFLAGS += $(TARGET_CFLAGS)
+
+ifeq ($(BR2_PACKAGE_DHRYSTONE_STATIC),y)
+DHRYSTONE_CFLAGS += -static
+endif
+
 define DHRYSTONE_EXTRACT_CMDS
 	(cd $(@D) && $(SHELL) $(DHRYSTONE_DL_DIR)/$($(PKG)_SOURCE))
 	$(Q)cp package/dhrystone/Makefile $(@D)/
 endef
 
 define DHRYSTONE_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) CFLAGS="$(DHRYSTONE_CFLAGS)"
 endef
 
 define DHRYSTONE_INSTALL_TARGET_CMDS
